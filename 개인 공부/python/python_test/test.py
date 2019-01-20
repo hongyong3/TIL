89460b5412f4b47bfb1900f15e6f8d444afb8282
hongyong3 <chy66822495@gmail.com>
2019-01-20 23:07:10 +0900

    D13 | 20190120 PM | python_problem

--- 개인 공부/python/python_test/test.py
+++ 개인 공부/python/python_test/test.py
@@ -1,5 +1,5 @@
-num = int(input("숫자를 넣어주세요."))
-if num%2:
-    print("홀수")
-else:
-    print("짝수")
+def my_round(float):
+    int_float = int(float)
+    return int_float + int((float -int_float)*2)
+print(my_round(3.2))
+print(my_round(3.6))
