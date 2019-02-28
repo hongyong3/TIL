8b2b63edb5c97f8dee66b9e56d26326a852c75fc
최홍용 <chy66822495@gmail.com>
2019-02-28 16:27:50 +0900

    D52| 20190228PM | Web_Evaluation_preparation

--- Algorithm/D-13/[TST]같은 모양 찾기 simple.py
+++ Algorithm/D-13/[TST]같은 모양 찾기 simple.py
@@ -2,10 +2,17 @@ import sys
 sys.stdin = open('같은 모양 찾기_input.txt', 'r')
 T = int(input())
 data = [list(map(int, input())) for i in range(T)]
-print(data)
 X = int(input())
 ans = [list(map(int, input())) for i in range(X)]
-print(ans)
 count = 0
-# for i in range(len(data)):
-#     for j in range(len(data)):
+stack = []
+
+for y in range(0,T-X+1):
+    for x in range(0,T-X+1):
+        for i in range(X):
+            stack.append(data[y+i][x:x +X])
+        if stack == ans:
+            count += 1
+        stack = []
+
+print(count)
