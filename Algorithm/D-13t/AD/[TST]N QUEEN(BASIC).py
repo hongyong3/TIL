84688fa5a892acfb1c0ff922a052c5bcdf780aa8
hongyong3 <chy66822495@gmail.com>
2019-04-02 19:57:49 +0900

    D85 | 20190402 PM | Algorithm_practice

--- Algorithm/D-13t/AD/[TST]N QUEEN(BASIC).py
+++ Algorithm/D-13t/AD/[TST]N QUEEN(BASIC).py
@@ -1,8 +1,10 @@
+import sys
+sys.stdin = open("[TST]N QUEEN(BASIC)_input.txt", "r")
+
 def nqueen(sol, N):
     global count
     if len(sol) == N:
         count += 1
-        print(count, sol)
         return 0
     candidate = list(range(N))
     for i in range(len(sol)):
@@ -21,23 +23,7 @@ def nqueen(sol, N):
     else:
         return 0
 
-import sys
-sys.stdin = open("N-QUEEN_input.txt", "r")
-T = int(input())
 count = 0
-for test_case in range(T):
-    N = int(input())
-    table = [[0 for _ in range(8)] for _ in range(8)]
-    # nqueen([test_case], N)
-if count == 0:
-    print("No solution")
-else:
-    print(count)
-
-# import sys
-# sys.stdin = open("N-QUEEN_input.txt", "r")
-# T = int(input())
-# for test_case in range(T):
-#     N = int(input())
-#     table = [[0 for _ in range(8)] for _ in range(8)]
-#
+N = int(input())
+table = [[0 for _ in range(4)] for _ in range(4)]
+print(count)
