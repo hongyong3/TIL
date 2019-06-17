5dc50baa7d6c4f354d3108dc7873353fb0f3c853
hongyong3 <chy66822495@gmail.com>
2019-06-17 23:03:43 +0900

    D153 | 20190617 PM | Java_Algorithm

--- Java/Inflearn/Eclipse_Java_Code/BAEKJOON/src/StepByStep_Solving_2/Lev2_8.java
+++ Java/Inflearn/Eclipse_Java_Code/BAEKJOON/src/StepByStep_Solving_2/Lev2_8.java
@@ -1,10 +1,37 @@
 package StepByStep_Solving_2;
 
+import java.util.Scanner;
+
 public class Lev2_8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		int a = keyboard.nextInt();
+		keyboard.close();
 
+		if (a % 5 == 0) {
+			System.out.println(a / 5);
+			return;
+		}
+		else {
+			int resultA = a / 5;
+
+			for (int i = resultA; i > 0; i--) {
+				int ans = a - (i * 5);
+
+				if ( ans % 3 == 0) {
+					System.out.println(i + (ans / 3));
+					return;
+				}
+			}
+		}
+
+		if (a % 3 == 0) {
+			System.out.println(a / 3);
+		}
+		else {
+			System.out.println(-1);
+		}
+		return;
 	}
-
 }
