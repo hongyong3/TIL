e52b06db04bfb491dc450540b4c625dbbf8e4787
hongyong3 <chy66822495@gmail.com>
2019-07-30 02:52:51 +0900

    D194 | 20190730 AM | Algorithm

--- Algorithm/Swea/D2_1983.py
+++ Algorithm/Swea/D2_1983.py
@@ -3,9 +3,12 @@ sys.stdin = open("D2_1983_input.txt", "r")
 
 T = int(input())
 for test_case in range(T):
-    avg = 0
-    N, M = list(map(int, input().split()))
+    grades = ["A+","A0","A-","B+","B0","B-","C+","C0","C-","D0"]
+    N, K = list(map(int, input().split()))
+    avg = [0] * N
     data = [list(map(int, input().split())) for _ in range(N)]
     for i in range(N):
-        avg = int((data[M][0] * 0.35 + data[M][1] * 0.45 + data[M][2] * 0.2))
-        if avg 
+        avg[i] = int((data[i][0] * 35 + data[i][1] * 45 + data[i][2] * 20))
+    for j in range(N):
+        if avg[K-1] == list(reversed(sorted(avg)))[j]:
+            print("#{} {}".format(test_case + 1, grades[int(j / (N / 10))]))
