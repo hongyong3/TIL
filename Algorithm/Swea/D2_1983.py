db8790a3785ab6f76e1055618101c053f938f92e
hongyong3 <chy66822495@gmail.com>
2019-07-29 23:20:58 +0900

    D193 | 20190729 PM | Algorithm

--- Algorithm/Swea/D2_1983.py
+++ Algorithm/Swea/D2_1983.py
@@ -3,5 +3,9 @@ sys.stdin = open("D2_1983_input.txt", "r")
 
 T = int(input())
 for test_case in range(T):
-    N, K = list(int(input().split()))
-    data = list(map(int, input().split()))
+    avg = 0
+    N, M = list(map(int, input().split()))
+    data = [list(map(int, input().split())) for _ in range(N)]
+    for i in range(N):
+        avg = int((data[M][0] * 0.35 + data[M][1] * 0.45 + data[M][2] * 0.2))
+        if avg 
