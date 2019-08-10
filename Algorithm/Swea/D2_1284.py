97b7ae65c446409ed688554c368473482845ee9c
hongyong3 <chy66822495@gmail.com>
2019-08-10 21:09:53 +0900

    D204 | 20190810 PM | Algorithm

--- Algorithm/Swea/D2_1284.py
+++ Algorithm/Swea/D2_1284.py
@@ -4,12 +4,8 @@ sys.stdin = open("D2_1284_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     P, Q, R, S, W = map(int, input().split())
-    cost = [0, 0]   # cost[0] = A_company | cost[1] = B_company
-
-    # A_company
+    cost = [0, 0]
     cost[0] = P * W
-
-    # B_company
     if R >= W:
         cost[1] = Q
     else:
