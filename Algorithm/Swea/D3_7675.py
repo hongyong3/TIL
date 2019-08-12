4a1fe8ccb6319be1412a9bdb286cca2c6115be2a
hongyong3 <chy66822495@gmail.com>
2019-08-12 17:58:01 +0900

    D206 | 20190812 PM | Algorithm

--- Algorithm/Swea/D3_7675.py
+++ Algorithm/Swea/D3_7675.py
@@ -5,18 +5,22 @@ T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(str, input().split()))
-    print(data)
     ans = [0] * N
     k = 0
     for i in range(len(data)):
-        if data[i][0].isupper():
-            if data[i][-1].islower():
-                ans[k] += 1
+        for j in range(len(data[i])):
+            if data[i].isalpha() or data[i][-1] == "." or data[i][-1] == "?" or data[i][-1] == "!":
+            if data[i][0].isupper():
+                if data[i][-1].islower():
+                    ans[k] += 1
             elif len(data[i]) == 1:
                 ans[k] += 1
             elif data[i][-1] == "." or data[i][-1] == "?" or data[i][-1] == "!":
-                if data[i][-2].islower():
-                    ans[k] += 1
+                if data[i][1: -2].isalpha():
+                    if data[i][-2].islower():
+                        ans[k] += 1
+                        k += 1
+                else:
                     k += 1
             else:
                 continue
