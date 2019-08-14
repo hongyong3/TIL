e8d28ed7849a4198811c8766715de9b29d8e827b
hongyong3 <chy66822495@gmail.com>
2019-08-14 23:43:47 +0900

    D208 | 20190814 PM | Final_Project_PPT

--- Algorithm/Swea/D3_7675.py
+++ Algorithm/Swea/D3_7675.py
@@ -14,3 +14,28 @@ for test_case in range(T):
             res += str(count) + ' '
             count = 0
     print("#{} {}".format(test_case + 1, res))
+
+
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = list(map(str, input().split()))
+#     ans = [0] * N
+#     k = 0
+#     for i in range(len(data)):
+#         if data[i].isalpha() and data[i][0].isupper():
+#             if len(data[i]) == 1:
+#                 ans[k] == 1
+#             else:
+#                 if data[i][1:].islower():
+#                     ans[k] += 1
+#                     continue
+#         if data[i][0].isupper() and (data[i][-1] == "." or data[i][-1] == "?" or data[i][-1] == "!"):
+#             if data[i][1: -1].isalpha():
+#                 if not data[i][1: -1].isupper() and data[i][1: -1].islower():
+#                     ans[k] += 1
+#                     k += 1
+#             else:
+#                 k += 1
+#     print("#{} ".format(test_case + 1), end="")
+#     print(*ans)
