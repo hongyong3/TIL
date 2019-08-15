a6ba59c8743d896712c1694baeabf4c9c7d4add4
hongyong3 <chy66822495@gmail.com>
2019-08-16 01:07:42 +0900

    D210 | 20190816 AM | Algorithm

--- Algorithm/Swea/D3_6019.py
+++ Algorithm/Swea/D3_6019.py
@@ -4,6 +4,6 @@ sys.stdin = open("D3_6019_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     D, A, B, F = map(float, input().split())
-    ans = D / float(A + B)
-    print("#{} {}".format(test_case + 1, float(ans)))
-    # print("#{} {}".format(test_case + 1, format(ans, "6f")))
+    time = D / (A + B)
+    ans = F * time
+    print("#{} {}".format(test_case + 1, format(float(ans), ".10f")))
--- Algorithm/Swea/D3_6057.py
+++ Algorithm/Swea/D3_6057.py
@@ -5,6 +5,18 @@ T = int(input())
 for test_case in range(T):
     N, M = map(int, input().split())
     data = [list(map(int, input().split())) for _ in range(M)]
-    temp = [[0] * N  for _ in range(2)]
+    temp = [[0] * (N) for _ in range(N)]
+    count = 0
+
     for i in range(N):
-        temp[data[i][0]]
+        temp[data[i][0] - 1][data[i][1] - 1] = 1
+        temp[data[i][1] - 1][data[i][0] - 1] = 1
+
+    for i in range(N):
+        for j in range(i + 1, N):
+            for k in range(j + 1, N):
+                if data[j][i] == 0: continue
+                if data[k][i] == 0: continue
+                if data[k][j] == 0: continue
+                count += 1
+    print("#{} {}".format(test_case + 1, count))
