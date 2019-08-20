f86d1d4deeac242a84ba440e604c07091f065224
최홍용 <chy2495@naver.com>
2019-08-20 18:02:58 +0900

    D213 | 20190820 PM | Algorithm

--- Algorithm/Swea/D3_5356.py
+++ Algorithm/Swea/D3_5356.py
@@ -4,10 +4,11 @@ sys.stdin = open("D3_5356_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     data = [list(map(str, input())) for _ in range(5)]
-    ans = ""
-    # print("#{} ".format(test_case + 1), end="")
+    temp = [[] * 5 for _ in range(15)]
+    print("#{} ".format(test_case + 1), end="")
     for i in range(5):
-        for j in data[i]:
-            ans += j
-    print(ans)
-            # print(data[j][i], end="")
+        for j in range(len(data[i])):
+            temp[j].append(data[i][j])
+    for i in range(len(temp)):
+        print(''.join(temp[i]), end="")
+    print()
