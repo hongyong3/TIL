dab6dc37afdd08d64e6a1fa93db5d52b0d479a38
hongyong3 <chy66822495@gmail.com>
2019-08-23 02:46:33 +0900

    D216 | 20190823 AM | Algorithm

--- Algorithm/Swea/D3_4579.py
+++ Algorithm/Swea/D3_4579.py
@@ -1,11 +1,11 @@
 import sys
-sys.stdin = open("D3_4522_input.txt", "r")
+sys.stdin = open("D3_4579_input.txt", "r")
 
 T = int(input())
 for test_case in range(T):
     data = str(input())
     result = "Exist"
     for i in range(int(len(data) / 2)):
-        if data[i] == "?" or data[len(data) - 1 - i] == "?":result
+        if data[i] == "*" or data[len(data) - 1 - i] == "*":result
         elif data[i] != data[len(data) - 1 - i]:result = "Not exist"
     print("#{} {}".format(test_case + 1, result))
