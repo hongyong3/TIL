332020343a843604f823ae1a66af5452e890357e
hongyong3 <chy66822495@gmail.com>
2019-08-24 22:34:08 +0900

    D217 | 20190824 PM | Algorithm

--- Algorithm/Swea/D3_3975.py
+++ Algorithm/Swea/D3_3975.py
@@ -7,5 +7,3 @@ for test_case in range(T):
     A, B, C, D = map(int, input().split())
     ans = ("ALICE" if A/B > C/D else "DRAW" if A/B == C/D else "BOB")
     print("#{} {}".format(test_case + +1, ans))
-    # for i in range(len(ans)):
-    #     print("#{} {}".format(test_case + +1, ans[i]))
