d8935f4941ea71ce1cb1ea9668554647c29a6ea7
hongyong3 <chy66822495@gmail.com>
2019-08-27 04:14:33 +0900

    D220 | 20190827 AM | Algorithm

--- Algorithm/Swea/1767.py
+++ Algorithm/Swea/1767.py
@@ -1,91 +1,63 @@
 import sys
 sys.stdin = open("1767_input.txt", "r")
-from collections import deque
+
+dx = [-1, 1, 0, 0]  # 상 하 좌 우
+dy = [0, 0, -1, 1]  # 상 하 좌 우
+
+def runCore(x, y, i, depth):    # 전선 길이구하는 함수
+    global N
+    count = 0
+    while 1:
+        x += dx[i]
+        y += dy[i]
+        if not (0 <= x < N and 0 <= y < N):
+            return count
+        if data[x][y] != 0:
+            return 0
+        count += 1
+        data[x][y] = 99 + depth
 
 
+def lineClear(x, y, i, depth):  # cell 초기화 시키는 함수
+    global N
+    while 1:
+        x += dx[i]
+        y += dy[i]
+        if not (0 <= x < N and 0 <= y < N):
+            return
+        if data[x][y] != 99 + depth:
+            return
+        data[x][y] = 0
 
-def find_core(st,y,x) :
-    if st == "up" :
-        for dn in range(y) :
-            if data[dn][x] == 1 : return True
-    elif st== 'dw' :
-        for dn in range(y+1,N) :
-            if data[dn][x] == 1 : return True
-    elif st== 'lf' :
-        for dm in range(x) :
-            if data[y][dm] == 1 : return True
-    else :
-        for dm in range(x+1,N) :
-            if data[y][dm] == 1 : return True
-    return False
+def dfs(depth, total, K):
+    global count, maxCore, minSum
 
-def find_restrict(sy,y,x,cy,cx) :
-    if st == 'up':
-        if cy < y and cx < x : return (cy,cx,"rg")
-        elif cy < y and cx > x : return (cy,cx,'lf')
-    elif st == 'dw':
-        if cy > y and cx < x: return (cy, cx, "rg")
-        elif cy > y and cx > x: return (cy, cx, 'lf')
-    elif st == 'lf':
-        if cx < x and cy < y : return (cy,cx,"dw")
-        elif cx < x and cy>y : return (cy,cx,"up")
-    else:
-        if cx > x and cy < y : return (cy,cx,"dw")
-        elif cx > x and cy > y : return (cy,cx,"up")
-    return None
+    if depth == count:
+        if K == maxCore:
+            minSum = min(minSum, total)
+        elif K > maxCore:
+            maxCore = K
+            minSum = total
+        return
+    x, y = ans[depth]
 
+    for i in range(4):
+        lineCount = runCore(x, y, i, depth)
+        if lineCount:
+            dfs(depth + 1, total + lineCount, K + 1)
+        lineClear(x, y, i, depth)   # 초기화
+    dfs(depth + 1, total, K)
 
 T = int(input())
-for t in range(T) :
+for test_case in range(1):
     N = int(input())
-
     data = [list(map(int, input().split())) for _ in range(N)]
-    cores = deque((i,j) for i in range(N) for j in range(N) if data[i][j]==1)
+    ans, count, maxCore, minSum = [], 0, 0, 999999999  # data[i][j] == 1, 코어의 수, 최대연결코어수, 전선의 길이 최소값
 
-    graph = dict()
-    for y,x in cores :
-
-        if y==0 or x==0 or x==N-1 or y==N-1 :
-            graph[(y,x)] = [("nr",0,[])]
-            continue
-        else : # 가지 않거나 가거나
-            graph[(y,x)] = [("ng",0,[])]
-            for st in "up", "dw", 'lf', 'rg' :
-                if find_core(st, y, x) : continue
-                temps = list()
-                for cy,cx in cores :
-                    if cy!=y and cx!=x :
-                        if find_restrict(st, y, x, cy, cx) : temps.append(find_restrict(st, y, x, cy, cx))
-                if st =="up" :
-                    graph[(y,x)].append( ("up",y,temps))
-                elif st =="dw" : graph[(y, x)].append(("dw", N - y - 1, temps))
-                elif st == 'lf' : graph[(y,x)].append( ("lf",x,temps) )
-                else :  graph[(y,x)].append( ("rg",N-x-1,temps))
-
-
-    paths = deque()
-    y,x = cores[0]
-    for status, price, restrict in graph[(y,x)] :
-        paths.append( [(y,x,status,price)] )
-
-    result = list()
-    while paths :
-        path = paths.popleft()
-        only_path = [(y,x,status) for y,x,status,price in path]
-        y,x,status,price = path[-1]
-
-        try :
-            ny,nx = cores[cores.index((y,x))+1] # 다음 갈 곳 _ path에 추가할 것
-            for nstatus, nprice, nrestricts in graph[(ny,nx)] :
-                temp = 1
-                for ry,rx,rs in nrestricts :
-                    if (ry,rx,rs) in only_path : temp=0 ; break
-                if temp :
-                    paths.append(path+[(ny,nx,nstatus,nprice)])
-        except : result.append(path)
-
-    for i in range(len(result)) :
-        result[i] = [pair[3] for pair in result[i] if pair[2]!='ng']
-    result.sort(key=lambda x:(-len(x),sum(x)))
-
-    print("#{} {}".format(t+1,sum(result[0])))
+    for i in range(1, N - 1):
+        for j in range(1, N - 1):
+            if data[i][j]:
+                ans.append([i, j])
+                count += 1  # 코어의 수 // 전체 코어의 수는 아니라 가장자리를 제외한 코어의 수
+    dfs(0, 0, 0)    # 코어 순서, 전선의 길이, 코어의 수
+    print("#{} {}".format(test_case + 1, minSum))
