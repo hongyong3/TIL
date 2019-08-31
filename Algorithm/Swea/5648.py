3baf9b3e77d0bb25d1cec8957a4c3d7ca87b6b32
hongyong3 <chy66822495@gmail.com>
2019-08-31 19:14:08 +0900

    D224 | 20190831 PM | Algorithm

--- Algorithm/Swea/5648.py
+++ Algorithm/Swea/5648.py
@@ -2,18 +2,38 @@ import sys
 sys.stdin = open("5648_input.txt", "r")
 
 
-dy = [-1, 1, 0, 0]  # 상 하 좌 우
-dx = [0, 0, -1, 1]  # 상 하 좌 우
-
 T = int(input())
-for test_case in range(1):
+for test_case in range(T):
     N = int(input())
     atom = [list(map(int, input().split())) for _ in range(N)]
-    arr = [[0] * 20 for _ in range(20)]         # 테스트
-    # arr = [[0] * 2000 for _ in range(2000)]   # 실제
+    atoms, ans, count = {}, 0, 0
+
     for i in range(len(atom)):
-        arr[atom[i][1] + 9][atom[i][0] + 9] = atom[i][3]      # 테스트
-        # arr[atom[i][1] + 999][atom[i][0] + 999] = atom[i][3]  # 실제
-    for i in arr:
-        print(i)
-    # print(map)
+        atoms[(atom[i][0] * 2, atom[i][1] * 2)] = [[atom[i][2]], [atom[i][3]]]  # 0.5초이므로 * 2
+    while count < 4000 and len(atom) > 1:
+        dy = [1, -1, 0, 0]  # 상 하 좌 우
+        dx = [0, 0, -1, 1]  # 상 하 좌 우
+        new = {}
+        for i in atoms.keys():
+            if atoms[i] == 0:
+                continue
+            x = i[0]
+            y = i[1]
+            dir = atoms[i][0][0]
+            energy = atoms[i][1][0]
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+            if nx < -2005 or ny < -2005 or nx > 2005 or ny > 2005: continue
+            if (nx, ny) in new:
+                new[(nx, ny)][0].append(dir)
+                new[(nx, ny)][1].append(energy)
+            else:
+                new[(nx, ny)] = [[dir], [energy]]
+
+        for i in new.keys():
+            if len(new[i][1]) > 1:
+                ans += sum(new[i][1])
+                new[i] = 0
+        atoms = new
+        count += 1
+    print("#{} {}".format(test_case + 1, ans))
