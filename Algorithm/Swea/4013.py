050fd2340d5a798fecb6495bb4b5970d8e1f7fc1
hongyong3 <chy66822495@gmail.com>
2019-09-16 23:51:19 +0900

    D237 | 20190916 PM | Algorithm

--- Algorithm/Swea/4013.py
+++ Algorithm/Swea/4013.py
@@ -3,14 +3,18 @@ sys.stdin = open("4013_input.txt", "r")
 
 def dfs(idx, dir):
     visited[idx] = 1
-    if (idx >= 0):   # 수정 필요
+
+    if (idx >= 0):   # 수정 필요 무한루프
         if data[idx][6] != data[idx - 1][2] and visited[idx - 1] == 0:
             dfs(i - 1, - dir)
+
     if (idx < 3):
         if data[idx][2] != data[idx + 1][6] and visited[idx + 1] == 0:
             dfs(idx + 1, - dir)
+
     if dir == 1:
         data[idx] = [data[idx][-1]] + data[idx][:-1]
+
     else:
         data[idx] = data[idx][1:] + [data[idx][0]]
 
