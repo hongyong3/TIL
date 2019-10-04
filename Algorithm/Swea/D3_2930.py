d9df0a8dd37c7e2d3013fee00a76fdad3cc9b7db
hongyong3 <chy66822495@gmail.com>
2019-10-04 23:27:40 +0900

    D255 | 20191004 AM | Algorithm

--- Algorithm/Swea/D3_2930.py
+++ Algorithm/Swea/D3_2930.py
@@ -15,3 +15,19 @@ for test_case in range(T):
             else:
                 answer.append(ans.pop(ans.index(max(ans))))
     print("#{}".format(test_case + 1), *answer)
+
+
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = [list(map(int, input().split())) for _ in range(N)]
+#     ans, answer = [], []
+#     for i in range(N):
+#         if len(data[i]) == 2:
+#             ans.append(data[i][1])
+#         else:
+#             if len(ans) == 0:
+#                 answer.append(-1)
+#             else:
+#                 answer.append(ans.pop(ans.index(max(ans))))
+#     print("#{}".format(test_case + 1), *answer)
