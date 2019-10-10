63c672cba69096c2a0288c0733cd2780d324926d
hongyong3 <chy66822495@gmail.com>
2019-10-10 22:47:18 +0900

    D260 | 20191010 PM | Algorithm

---
+++ Algorithm/Swea/D4_6109.py
@@ -0,0 +1,116 @@
+import sys
+sys.stdin = open("D4_6109_input.txt", "r")
+
+def upGame(x, y):
+    ny = y - 1
+    if ny >= 0:
+        if data[ny][x] > 0:
+            if data[ny][x] == data[y][x]:
+                data[ny][x] == data[y][x] * 2
+                data[y][x] = - 1
+                return
+            else:
+                return
+        elif data[ny][x] == - 1:
+            data[ny][x] = data[y][x]
+            data[y][x] = 0
+            return
+        else:
+            data[ny][x] = data[y][x]
+            data[y][x] = 0
+            upGame(x, ny)
+
+def downGame(x, y):
+    ny = y + 1
+    if ny < int(N):
+        if data[ny][x] > 0:
+            if data[ny][x] == data[y][x]:
+                data[ny][x] == data[y][x] * 2
+                data[y][x] = - 1
+                return
+            else:
+                return
+        elif data[ny][x] == - 1:
+            data[ny][x] = data[y][x]
+            data[y][x] = 0
+            return
+        else:
+            data[ny][x] = data[y][x]
+            data[y][x] = 0
+            upGame(x, ny)
+
+def leftGame(x, y):
+    nx = x - 1
+    if nx >= 0:
+        if data[y][nx] > 0:
+            if data[y][nx] == data[y][x]:
+                data[y][nx] == data[y][x] * 2
+                data[y][x] = - 1
+                return
+            else:
+                return
+        elif data[y][nx] == - 1:
+            data[y][nx] = data[y][x]
+            data[y][x] = 0
+            return
+        else:
+            data[y][nx] = data[y][x]
+            data[y][x] = 0
+            upGame(nx, y)
+
+def rightGame(x, y):
+    nx = x + 1
+    if nx < int(N):
+        if data[y][nx] > 0:
+            if data[y][nx] == data[y][x]:
+                data[y][nx] == data[y][x] * 2
+                data[y][x] = - 1
+                return
+            else:
+                return
+        elif data[y][nx] == - 1:
+            data[y][nx] = data[y][x]
+            data[y][x] = 0
+            return
+        else:
+            data[y][nx] = data[y][x]
+            data[y][x] = 0
+            upGame(nx, y)
+
+T = int(input())
+for test_case in range(T):
+    N, S = map(str, input().split())
+    data = [list(map(int, input().split())) for _ in range(int(N))]
+
+    if S == 'up':
+        for j in range(int(N)):
+            for i in range(int(N)):
+                if data[j][i]:
+                    upGame(i, j)
+
+    if S == 'down':
+        for j in range(int(N) - 1, - 1, - 1):
+            for i in range(int(N)):
+                if data[j][i]:
+                    downGame(i, j)
+
+    if S == 'left':
+        for i in range(int(N)):
+            for j in range(int(N)):
+                if data[j][i]:
+                    leftGame(i, j)
+
+    if S == 'right':
+        for i in range(int(N) - 1, - 1, - 1):
+            for j in range(int(N)):
+                if data[j][i]:
+                    rightGame(i, j)
+
+    for i in range(int(N)):
+        for j in range(int(N)):
+            if data[i][j] == - 1:
+                data[i][j] = 0
+
+    print("#{}".format(test_case + 1))
+    for i in range(int(N)):
+        print(' '.join(map(str, data[i])))
--- Algorithm/Swea/D4_6959.py
+++ Algorithm/Swea/D4_6959.py
@@ -3,45 +3,40 @@ sys.stdin = open("D4_6959_input.txt", "r")
 
 # def solve(data):
 #     global count
-#     ans = []
 #     if len(data) >= 3:
-#         for i in range(len(data) - 2):
-#             ans += data[i]
-#     ans += str(int(data[- 2]) + int(data[- 1]))
-#     count += 1
-#     if len(ans) > 1:
+#         ans = str(data[0: len(data) - 2]) + str(int(data[- 2]) + int(data[- 1]))
+#         count += 1
 #         solve(ans)
+#     elif len(data) == 2:
+#         ans = str(int(data[- 2]) + int(data[- 1]))
+#         count += 1
+#         solve(ans)
+#     return count
 #
 # T = int(input())
 # for test_case in range(T):
 #     data = str(input())
+#     if len(data) == 1:
+#         print("#{} {}".format(test_case + 1, "B"))
+#         continue
 #     count = 0
-#     if len(data) < 2:
-#         print("#{} {}".format(test_case + 1, 'B'))
+#     solve(data)
+#     if count % 2:
+#         print("#{} {}".format(test_case + 1, "A"))
 #     else:
-#         solve(data)
-#         answer = "B"
-#         if count % 2: answer = "A"
-#         print("#{} {}". format(test_case + 1, answer))
-def solve(data):
-    global count
-    if len(data) == 1:
-        return count
-    if len(data) >= 3:
-        ans = str(data[0: len(data) - 2]) + str(int(data[- 2]) + int(data[- 1]))
-        count += 1
-        solve(ans)
-    else:
-        ans = str(int(data[- 2]) + int(data[- 1]))
-        count += 1
-        solve(ans)
+#         print("#{} {}".format(test_case + 1, "B"))
 
 T = int(input())
 for test_case in range(T):
     data = str(input())
-    count = 0
-    solve(data)
+    ans, count = 0, 0
+    for i in range(len(data)):
+        ans += int(data[i])
+        count += 1
+        if ans >= 10:
+            ans = ans // 10 + ans % 10
+            count += 1
     if count % 2:
-        print("#{} {}".format(test_case + 1, "A"))
-    else:
         print("#{} {}".format(test_case + 1, "B"))
+    else:
+        print("#{} {}".format(test_case + 1, "A"))
