59d1f408c5a805658e05aa77a0414c03aee8bc84
hongyong3 <chy66822495@gmail.com>
2019-10-25 21:59:33 +0900

    D274 | 20191025 PM | Algorithm

--- Algorithm/Swea/D4_7701.py
+++ Algorithm/Swea/D4_7701.py
@@ -1,20 +1,5 @@
 import sys
 sys.stdin = open("D4_7701_input.txt", "r")
-
-# T = int(input())
-# for test_case in range(T):
-#     N = int(input())
-#     data = [list(map(str, input().split())) for _ in range(N)]
-#     ans = []
-#     data = sorted(data)
-#     print("#{}".format(test_case + 1))
-#     for j in range(len(data)):
-#         if data[j] not in ans:
-#             ans.append(data[j])
-#     for k in ans: print(*k)
-​
-#########################################################
-
 # def solve(data):
 #     return sorted(sorted(data), key = lambda x : x[0])
 #
