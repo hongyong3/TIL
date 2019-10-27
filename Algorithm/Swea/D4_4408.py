850eecc4f8a9cc1f88138c29fa4d7cdfa53a1248
hongyong3 <chy66822495@gmail.com>
2019-10-28 02:50:28 +0900

    D277 | 20191028 AM | Algorithm

--- Algorithm/Swea/D4_4408.py
+++ Algorithm/Swea/D4_4408.py
@@ -2,13 +2,14 @@ import sys
 sys.stdin = open("D4_4408_input.txt", "r")
 
 T = int(input())
-for test_case in range(1):
+for test_case in range(T):
     N = int(input())
-    data = [list(map(int, input().split())) for _ in range(N)]
-    # room = [[0] * 200 for _ in range(200)]
-    room1 = [0] * 200
-    # print(room1[data[0][0]: data[0][1]])
-
+    room, ans = [0] * 201, 0
     for i in range(N):
-        room1[data[i][0]: data[i][1]] = 1
-    print(room1)
+        a, b = map(int, input().split())
+        if a % 2 == 1: a += 1
+        if b % 2 == 1: b += 1
+        if a > b: a, b = b, a
+        for j in range((a // 2), (b // 2) + 1):
+            room[j] += 1
+    print("#{} {}".format(test_case + 1, max(room)))
