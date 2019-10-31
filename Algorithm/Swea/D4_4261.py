841ff2abdc81fd5a50410fb37b4a481dbf23d7d2
hongyong3 <chy66822495@gmail.com>
2019-10-31 23:41:57 +0900

    D280 | 20191031 PM | Algorithm

--- Algorithm/Swea/D4_4261.py
+++ Algorithm/Swea/D4_4261.py
@@ -1,9 +1,9 @@
 import sys
 sys.stdin = open("D4_4261_input.txt", "r")
 
+
+keypad = {{"2": "a", "b", "c"}, ['3': 'd', 'e', 'f'], {'4': "g", "h", "i"}, {'5': "j", "k", "l"}, {"6": "m", "n", "o"}, {"7":}, {}, {}}
 T = int(input())
 for test_case in range(T):
     S, N = map(int, input().split())
     data = list(map(str, input().split()))
-    print(S, N)
-    print(data)
