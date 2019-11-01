a8a08fd5f406e042382763de2b1e08e1b71d7f7e
hongyong3 <45934494+hongyong3@users.noreply.github.com>
2019-11-01 21:58:05 +0900

    D281 | 20191101 PM | Algorithm

--- Algorithm/Swea/D4_4261.py
+++ Algorithm/Swea/D4_4261.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("D4_4261_input.txt", "r")
 
 
-keypad = {{"2": "a", "b", "c"}, ['3': 'd', 'e', 'f'], {'4': "g", "h", "i"}, {'5': "j", "k", "l"}, {"6": "m", "n", "o"}, {"7":}, {}, {}}
+keypad = {{"2": "a", "b", "c"}, ['3': 'd', 'e', 'f'], {'4': "g", "h", "i"}, {'5': "j", "k", "l"}, {"6": "m", "n", "o"}, {"7":`p`}, {}, {}}
 T = int(input())
 for test_case in range(T):
     S, N = map(int, input().split())
