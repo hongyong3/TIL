9fd24faa7be26537a991d12cc0951cd579760654
hongyong3 <chy66822495@gmail.com>
2019-11-07 22:47:56 +0900

    D286 | 20191107 PM | Algorithm

--- Algorithm/Swea/D4_3459.py
+++ Algorithm/Swea/D4_3459.py
@@ -1,24 +1,13 @@
 import sys
 sys.stdin = open("D4_3459_input.txt", "r")
 
-def solve(N, x):
-    global answer
-    if N <= x:
-        return answer
-    if (N >= 2 * x + 1):
-        x = 2 * x + 1
-        answer += 1
-        solve(N, x)
-    elif (N >= 2 * x):
-        x = 2 * x
-        answer += 1
-        solve(N, x)
-    elif (N < 2 * x) or (N < 2 * x + 1):
-        return answer
-
 T = int(input())
-for test_case in range(1):
+for test_case in range(T):
     N = int(input())
-    ans, answer = ["Alice", "Bob"], 1
-    solve(N, 1)
-    print("#{} {}".format(test_case + 1, ans[answer % 2]))
+    ans = "Alice"
+    while N >= 10:
+        N = (N // 2) + 1
+        N = (N // 2) - 1
+    if N == 1 or (6 <= N <= 9):
+        ans = "Bob"
+    print("#{} {}".format(test_case + 1, ans))
