3167fa92866dc737bc72c2dbe1ba464d6e77f145
hongyong3 <chy66822495@gmail.com>
2019-11-12 22:48:21 +0900

    D291 | 20191112 PM | Algorithm

--- Algorithm/Swea/D4_3263.py
+++ Algorithm/Swea/D4_3263.py
@@ -1,20 +1,31 @@
 import sys
 sys.stdin = open("D4_3263_input.txt", "r")
 
+############################################# O(Nlog(N)
 T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
-    count, dp, array = 0, [0] * N, [0] * N
-    dp[0] = 1
-    for i in range(N):
-        dp[i] = 1
-        for j in range(i):
-            if array[j] < array[i] and dp[j] + 1> dp[i]:
-                dp[i] = dp[j] + 1
+    ans, DP = N, [0] * (N + 1)
+    for i in data:
+        DP[i] = DP[i - 1] + 1
+        ans = min(ans, N - DP[i])
+    print("#{} {}".format(test_case + 1, ans))
 
-    for i in range(1, N):
-        if count < dp[i]:
-            count = dp[i]
-    print(count)
-    # print(N - count)
+############################################# O(N^2) 시간초과
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = list(map(int, input().split()))
+#     count, dp = 0, [0] * N
+#     dp[0] = 1
+#     for i in range(1, N):
+#         dp[i] = 1
+#         for j in range(i):
+#             if data[j] < data[i] and dp[j] + 1> dp[i]:
+#                 dp[i] = dp[j] + 1
+#
+#     for i in range(1, N):
+#         if count < dp[i]:
+#             count = dp[i]
+#     print("#{} {}".format(test_case + 1, N - count))
