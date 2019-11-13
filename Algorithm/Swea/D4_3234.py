a190aa3cef8c9b12e6da392c7f08b45fb750064b
hongyong3 <chy66822495@gmail.com>
2019-11-13 17:14:21 +0900

    D292 | 20191113 PM | Algorithm

---
+++ Algorithm/Swea/D4_3234.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("D4_3234_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    data = list(map(int, input().split()))
+    ans = #2^N * N!
