8d25e1ffb89831e1e3c658fa8dd3b282a26477f9
hongyong3 <chy66822495@gmail.com>
2019-12-11 22:03:58 +0900

    D319 | 20191211 PM | Algorithm

--- Algorithm/Swea/D4_1824.py
+++ Algorithm/Swea/D4_1824.py
@@ -6,22 +6,28 @@ dy = [0, 0, - 1, 1]
 
 
 def solve(x, y):
-    global memory, nx, ny
+    global memory, dir
 
     if data[x][y] == '>' or '<' or '^' or 'v':
-        if data[x][y] == '>':
-            nx = x + dx[3]
-            ny = y + dy[3]
-        elif data[x][y] == '<':
-            nx = x + dx[2]
-            ny = y + dy[2]
-        elif data[x][y] == '^':
-            nx = x + dx[0]
-            ny = y + dy[0]
-        elif data[x][y] == 'v':
-            nx = x + dx[1]
-            ny = y + dy[1]
-        solve(nx, ny)
+        if data[x][y] == '>' or (data[x][y] == '_' and memory == 0):
+            dir = 3
+        elif data[x][y] == '<' or (data[x][y] == '_'):
+            dir = 2
+        elif data[x][y] == '^' or (data[x][y] == '|'):
+            dir = 0
+        elif data[x][y] == 'v' or (data[x][y] == '|' and memory == 0):
+            dir = 1
+        elif data[x][y] == '+':
+            memory = (memory + 1) % 16
+        elif data[x][y] == '-':
+            if memory == 0:
+                memory = 15
+            else:
+                memory = memory - 1
+        elif data[x][y] == '@':
+            return True
+
+
 
 
 T = int(input())
@@ -29,8 +35,9 @@ for test_case in range(T):
     R, C = map(int, input().split())
     data = [list(input()) for _ in range(R)]
     memory = int(data[0][0])
-    nx, ny = 0, 0
-    solve(0.1)
+    dir = 0
+    # nx, ny = 0, 0
+    solve(0, 1)
 
 
     print("#{} {}".format(test_case + 1, "YES"))
