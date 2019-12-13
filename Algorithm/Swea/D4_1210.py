6663f13fcc58615c8c8cfa30806741ffaa8cae32
hongyong3 <chy66822495@gmail.com>
2019-12-13 23:50:14 +0900

    D321 | 20191213 PM | Algorithm

---
+++ Algorithm/Swea/D4_1210.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open("D4_1210_input.txt", "r")
+
+dx = [0, 0, - 1]
+dy = [- 1, 1, 0]
+def check(x, y):
+    if not (0 <= x < 100 and 0 <= y < 100) or data[x][y] == 0 or data[x][y]  == 9:
+        return False
+    return True
+
+def dfs(x, y):
+    while True:
+        if x == 0:
+            return y
+        for i in range(3):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if check(nx, ny):
+                x = nx
+                y = ny
+                data[nx][ny] = 9
+
+for test_case in range(10):
+    N = int(input())
+    data = [list(map(int, input().split())) for _ in range(100)]
+    for i in range(99, - 1, - 1):
+        for j in range(99, - 1, - 1):
+            if data[i][j] == 2:
+                print("#{} {}".format(test_case + 1, dfs(i, j)))
+                break
