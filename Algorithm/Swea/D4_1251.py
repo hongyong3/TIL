5c037de8a76957fed2ce29311a82b12ca9e6a494
hongyong3 <chy66822495@gmail.com>
2020-01-07 23:49:37 +0900

    D346 | 20200107 PM | Algorithm

--- Algorithm/Swea/D4_1251.py
+++ Algorithm/Swea/D4_1251.py
@@ -1,9 +1,9 @@
 import sys
 sys.stdin = open("D4_1251_input.txt", "r")
 
+
+
 T = int(input())
-
-
 for test_case in range(2):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(2)]
