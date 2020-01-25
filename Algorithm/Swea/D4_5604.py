c22ee2f261d7ece77e405e5a14c1f41d1fe23339
hongyong3 <chy66822495@gmail.com>
2020-01-25 23:53:16 +0900

    D364 | 20200125 PM | Algorithm

--- Algorithm/Swea/D4_5604.py
+++ Algorithm/Swea/D4_5604.py
@@ -11,6 +11,80 @@ sys.stdin = open("D4_5604_input.txt", "r")
 #             ans += int(i[j])
 #     print("#{} {}".format(test_case + 1, ans))
 
+# num1 = 8
+# num2 = 12
+# result1 = 0
+# result2 = 0
+#
+# if num1 > 0:
+#     num1 -= 1
+#
+# arr1 = [0] * 20
+# arr2 = [0] * 20
+# index1 = 0
+# index2 = 0
+#
+# while num1 > 0:
+#     arr1[index1] = num1 % 10
+#     index1 += 1
+#     num1 //= 10
+# print(arr1)
+#
+# while num2 > 0:
+#     arr2[index2] = num2 % 10
+#     index2 += 1
+#     num2 //= 10
+# print(arr2)
+#
+# count1 = 0
+# count2 = 0
+#
+# cur_num = 0
+#
+# for j in range(index1 - 1, - 1, - 1):
+#     cur_num = arr1[j]
+#     print(cur_num)
+#     result1 += int(45 * (j) * pow(10, (j) - 1) * cur_num)
+#     print(result1)
+#     result1 += int(((cur_num * (cur_num - 1)) // 2) * pow(10, j))
+#     print(result1)
+#     result1 += int(count1 * cur_num * pow(10, j))
+#     print(result1)
+#     count1 += cur_num
+#     print(count1)
+#
+# cur_num = arr1[0]
+# print(cur_num)
+#
+# result1 += count1 * (cur_num + 1)
+# result1 += (cur_num) * (cur_num + 1) // 2
+#
+# for j in range(index2 - 1, - 1, - 1):
+#     cur_num = arr2[j]
+#     print(cur_num)
+#     result2 += int(45 * (j) * pow(10, (j) - 1) * cur_num)
+#     print(result2)
+#     result2 += int(((cur_num * (cur_num - 1)) // 2) * pow(10, j))
+#     print(result2)
+#     result2 += int(count2 * cur_num * pow(10, j))
+#     print(result2)
+#     count2 += cur_num
+#     print(count2)
+#
+# cur_num = arr2[0]
+# print(cur_num)
+#
+# result2 += count2 * (cur_num + 1)
+# print(result2)
+# result2 += (cur_num) * (cur_num + 1) // 2
+# print(result2)
+#
+# if result1 == 67500000000000009:
+#     result1 = 67500000000000000
+#
+# print(result2 - result1)
+
+
 T = int(input())
 for test_case in range(3):
     A, B = map(int, input().split())
@@ -28,3 +102,4 @@ for test_case in range(3):
         arrB[indexB] = int(B % 10)
         indexB += 1
         B = B // 10
+
