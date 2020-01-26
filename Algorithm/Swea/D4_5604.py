4e059f3b2c07a1f1c6ee3573c32de59aa1052946
hongyong3 <chy66822495@gmail.com>
2020-01-26 21:36:19 +0900

    D363 | 20200124 PM | Algorithm

--- Algorithm/Swea/D4_5604.py
+++ Algorithm/Swea/D4_5604.py
@@ -85,21 +85,21 @@ sys.stdin = open("D4_5604_input.txt", "r")
 # print(result2 - result1)
 
 
-T = int(input())
-for test_case in range(3):
-    A, B = map(int, input().split())
-    ansA, ansB = 0, 0
-    arrA, arrB = [0] * 20, [0] * 20
-    indexA, indexB = 0, 0
-    countA, countB = 0, 0
-
-    while A > 0:
-        arrA[indexA] = int(A % 10)
-        indexA += 1
-        A = A // 10
-
-    while B > 0:
-        arrB[indexB] = int(B % 10)
-        indexB += 1
-        B = B // 10
-
+# T = int(input())
+# for test_case in range(3):
+#     A, B = map(int, input().split())
+#     ansA, ansB = 0, 0
+#     arrA, arrB = [0] * 20, [0] * 20
+#     indexA, indexB = 0, 0
+#     countA, countB = 0, 0
+#
+#     while A > 0:
+#         arrA[indexA] = int(A % 10)
+#         indexA += 1
+#         A = A // 10
+#
+#     while B > 0:
+#         arrB[indexB] = int(B % 10)
+#         indexB += 1
+#         B = B // 10
+#
