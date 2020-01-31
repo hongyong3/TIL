f6e32116993768242aa3c7db9b61152d03a1a818
hongyong3 <chy66822495@gmail.com>
2020-01-31 21:25:52 +0900

    D369 | 20200131 PM | Algorithm

--- Algorithm/Swea/D4_5608.py
+++ Algorithm/Swea/D4_5608.py
@@ -1,29 +1,59 @@
 import sys
 sys.stdin = open("D4_5608_input.txt", "r")
 
+# def solve(n, idx):
+#     global count, num
+#     if idx < 0:
+#         return
+#
+#     if n >= arr[idx]:
+#         n -= arr[idx]
+#         a[arr[idx]] += 1
+#         count += 1
+#         num += arr[idx]
+#         solve(n, idx)
+#
+#     else:
+#         solve(n, idx - 1)
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#
+#     arr, a, count, num = [], {}, 0, 0
+#
+#     for i in range(2, N):
+#         if pow(i, 3) < N:
+#             arr.append(pow(i, 3))
+#             a[pow(i, 3)] = 0
+#         else:
+#             break
+#
+#     if arr:
+#         solve(N, len(arr) - 1)
+#         print(count + N - num + 1, num - 1)
+#     else:
+#         print(N, N)
+
+
+def solve(n):
+    memo = [0] * (n + 1)
+    memo[1] = 1
+    idx = 1
+    for i in range(2, n + 1):
+        if i in arr:
+            idx += 1
+        memo[i] = memo[i - arr[idx - 1]] + 1
+    return max(memo), len(memo) - 1 - memo[::-1].index(max(memo))
+
 T = int(input())
-for test_case in range(1):
-    # N = int(input())
-    N = 154
-
-    maxNum, maxCount, idx, arr = 0, 0, 1, [[0] * N, [0] * (N + 1)]
-
-    for i in range(1, N + 1):
+for test_case in range(T):
+    N = int(input())
+    arr = []
+    for i in range(3, N):
         if pow(i, 3) < N:
-            arr[0][i] = pow(i, 3)
+            arr.append(pow(i, 3))
         else:
             break
-
-    for i in range(1, N + 1):
-        if i == arr[0][idx]:
-            arr[1][i] = 1
-            idx += 1
-        else:
-            arr[1][i] = arr[1][i - arr[0][idx - 1]] + 1
-
-    for i in range(1, N + 1):
-        if maxCount <= arr[1][i]:
-            maxCount = arr[1][i]
-            maxNum = i
-    print("#{} {} {}".format(test_case + 1, maxNum, maxCount))
-    print(arr[1])
+    print(arr)
+    print("#{}".format(test_case + 1), *solve(N))
