70ef0df5c8f01022eafc57e29267d7fabd4314c4
hongyong3 <chy66822495@gmail.com>
2020-02-03 23:48:31 +0900

    D372 | 20200203 PM | Algorithm

--- Algorithm/Swea/D4_5643.py
+++ Algorithm/Swea/D4_5643.py
@@ -3,31 +3,31 @@ sys.stdin = open("D4_5643_input.txt", "r")
 
 # Floyd_Warshall Algorithm
 # 시간복잡도 O(V^3)
-
-T = int(input())
-for test_case in range(T):
-    N = int(input())
-    M = int(input())
-    data = [[0] * N for _ in range(N)]
-    ans = 0
-
-    for i in range(M):
-        a, b = map(int, input().split())
-        data[a - 1][b - 1] = 1
-
-    for m in range(N):
-        for s in range(N):
-            for e in range(N):
-                if data[s][e] == 1 or (data[s][m] == 1 and data[m][e] == 1):
-                    data[s][e] = 1
-
-    for s in range(N):
-        known_people = 0
-        for e in range(N):
-            known_people += data[s][e] + data[e][s]
-        if known_people == N - 1:
-            ans += 1
-    print("#{} {}".format(test_case + 1, ans))
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     M = int(input())
+#     data = [[0] * N for _ in range(N)]
+#     ans = 0
+#
+#     for _ in range(M):
+#         a, b = map(int, input().split())
+#         data[a - 1][b - 1] = 1
+#
+#     for m in range(N):
+#         for s in range(N):
+#             for e in range(N):
+#                 if data[s][e] == 1 or (data[s][m] == 1 and data[m][e] == 1):
+#                     data[s][e] = 1
+#
+#     for s in range(N):
+#         known_people = 0
+#         for e in range(N):
+#             known_people += data[s][e] + data[e][s]
+#         if known_people == N - 1:
+#             ans += 1
+#     print("#{} {}".format(test_case + 1, ans))
 
 # BFS로도 가능할 듯
 
@@ -35,3 +35,12 @@ T = int(input())
 for test_case in range(T):
     N = int(input())
     M = int(input())
+
+    dataT = [[] for _ in range(N)]
+    dataS = [[] for _ in range(N)]
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+        dataT[a - 1].append(b)
+        dataS[b - 1].append(a)
+    
