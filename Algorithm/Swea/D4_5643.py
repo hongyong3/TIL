3fcc1e203c57c40c046dc1058ec56ff2d9b21afa
hongyong3 <chy66822495@gmail.com>
2020-02-04 19:56:06 +0900

    D373 | 20200204 PM | Algorithm

--- Algorithm/Swea/D4_5643.py
+++ Algorithm/Swea/D4_5643.py
@@ -31,6 +31,23 @@ sys.stdin = open("D4_5643_input.txt", "r")
 
 # BFS로도 가능할 듯
 
+import collections
+
+def bfs(v, arr):
+    q = collections.deque()
+    subAns = 0
+    visited[v] = 1
+    q.append(v)
+
+    while q:
+        v = q.popleft()
+        for i in arr[v]:
+            visited[i] = 1
+            q.append(i)
+            subAns += 1
+
+    return subAns
+
 T = int(input())
 for test_case in range(T):
     N = int(input())
@@ -43,4 +60,8 @@ for test_case in range(T):
         a, b = map(int, input().split())
         dataT[a - 1].append(b)
         dataS[b - 1].append(a)
-    
+
+    ans = 0
+    for i in range(N):
+        visited = [0] * (N)
+        knownPeople = bfs(i, dataT) + bfs(i + dataS) + 1
