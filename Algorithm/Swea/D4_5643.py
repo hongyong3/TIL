19cda43f532f225a78950f54220f8c481c622bb4
hongyong3 <chy66822495@gmail.com>
2020-02-06 22:24:09 +0900

    D375 | 20200206 PM | Algorithm

--- Algorithm/Swea/D4_5643.py
+++ Algorithm/Swea/D4_5643.py
@@ -31,37 +31,37 @@ sys.stdin = open("D4_5643_input.txt", "r")
 
 # BFS로도 가능할 듯
 
-import collections
-
-def bfs(v, arr):
-    q = collections.deque()
-    subAns = 0
-    visited[v] = 1
-    q.append(v)
-
-    while q:
-        v = q.popleft()
-        for i in arr[v]:
-            visited[i] = 1
-            q.append(i)
-            subAns += 1
-
-    return subAns
-
-T = int(input())
-for test_case in range(T):
-    N = int(input())
-    M = int(input())
-
-    dataT = [[] for _ in range(N)]
-    dataS = [[] for _ in range(N)]
-
-    for _ in range(M):
-        a, b = map(int, input().split())
-        dataT[a - 1].append(b)
-        dataS[b - 1].append(a)
-
-    ans = 0
-    for i in range(N):
-        visited = [0] * (N)
-        knownPeople = bfs(i, dataT) + bfs(i, dataS) + 1
+# import collections
+#
+# def bfs(v, arr):
+#     q = collections.deque()
+#     subAns = 0
+#     visited[v] = 1
+#     q.append(v)
+#
+#     while q:
+#         v = q.popleft()
+#         for i in arr[v]:
+#             visited[i] = 1
+#             q.append(i)
+#             subAns += 1
+#
+#     return subAns
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     M = int(input())
+#
+#     dataT = [[] for _ in range(N)]
+#     dataS = [[] for _ in range(N)]
+#
+#     for _ in range(M):
+#         a, b = map(int, input().split())
+#         dataT[a - 1].append(b)
+#         dataS[b - 1].append(a)
+#
+#     ans = 0
+#     for i in range(N):
+#         visited = [0] * (N)
+#         knownPeople = bfs(i, dataT) + bfs(i, dataS) + 1
---
+++ Algorithm/Swea/D4_5671.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("D4_5671_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N, M = map(int, input().split())
+    word1 = input()
+    word2 = input()
