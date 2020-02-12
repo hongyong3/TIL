dc4f5ab64cea4b7329dce8eca808d91bd169e988
hongyong3 <chy66822495@gmail.com>
2020-02-12 19:53:10 +0900

    D381 | 20200212 PM | Algorithm

--- Algorithm/Swea/D4_5672.py
+++ Algorithm/Swea/D4_5672.py
@@ -12,20 +12,41 @@ for test_case in range(int(T)):
     for t in temporary:
         if t.isdigit():
             N += t
-    data = [input()[0] for _ in range(int(N))]
+    data = [ord(input()[0]) for _ in range(int(N))]
     newData = ''
+    print(data)
 
-    f, l = 0, - 1
-    while data:
-        if ord(data[f]) < ord(data[l]):
-            newData += data.pop(0)
-            f, l = 0, - 1
-        elif ord(data[f]) > ord(data[l]):
-            newData += data.pop(- 1)
-            f, l = 0, - 1
-        elif ord(data[f]) == ord(data[l]):
-            f += 1
-            l -= 1
-        if len(data) == 1:
-            newData += data.pop()
+    front, back = 0, N - 1
+    while front != back:
+        newFront, newBack = front, back
+
+        if data[front] < data[back]:
+            newData += chr(data[front])
+            newFront += 1
+
+        elif data[front] > data[back]:
+            newData += chr(data[back])
+            newBack -= 1
+
+        elif data[front] == data[back]:
+            subFront = front + 1
+            subBack = back - 1
+            same, different = False, False
+
+            while True:
+                if data[subFront] != data[subBack]:
+                    different = True
+                    break
+                else:
+                    if subFront + 1 <= subBack - 1:
+                        subFront += 1
+                        subBack -= 1
+                    else:
+                        same = True
+                        break
+
+            # if same == False:
+            #     if data[subFront] < data[subBack]:
+
+
     print("#{} {}".format(test_case + 1, newData))
