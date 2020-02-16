421969806ae04c54bf3db7a08373410314699317
hongyong3 <chy66822495@gmail.com>
2020-02-17 00:00:21 +0900

    D385 | 20200216 PM | Algorithm

--- Algorithm/Swea/D4_5676.py
+++ Algorithm/Swea/D4_5676.py
@@ -1,23 +1,49 @@
 import sys
 sys.stdin = open("D4_5676_input.txt", "r")
 
-def intersection(x1, y1, x2, y2, x3, y3, x4, y4):
+# def intersection(x3, y3, x4, y4):
+#     global meet, ans
+#     p = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
+#     tm = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)
+#     sm = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)
+#
+#     if tm == sm == p == 0:
+#         meet = 4
+#         return
+#
+#     if p:
+#         if 0 < tm / p < 1 and 0 < sm / p < 1:
+#                 t, s = tm / p, sm / p
+#                 x = x1 + t * (x2 - x1)
+#                 y = y1 + t * (y2 - y1)
+#                 if [x, y] not in ans:
+#                     ans.append([x, y])
+#
+# T = int(input().replace('혻', ''))
+# for test_case in range(T):
+#     xmin, ymin, xmax, ymax = map(int, input().replace('혻', '').split())
+#     x1, y1, x2, y2 = map(int, input().replace('혻', '').split())
+#
+#     meet = 0
+#     ans = []
+#     rectangleLine = [[xmin, ymin, xmax, ymin], [xmin, ymin, xmin, ymax], [xmin, ymax, xmax, ymax], [xmax, ymin, xmax, ymax]]
+#
+#     for i in rectangleLine:
+#         intersection(i[0], i[1], i[2], i[3])
+#     print("#{} {}".format(test_case + 1, meet))
+
+def intersection(x3, y3, x4, y4):
     global meet, ans
-    tm = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)
-    td = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-    sm = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)
-    sd = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
-
-
-    if td != 0 and sd != 0:
-        if 0 < tm // td < 1 and 0 < sm // sd < 1:
-            t, s = tm // td, sm // sd
-            x = x1 + s * (x2 - x1)
-            y = y1 + s * (y2 - y1)
-            if [x, y] not in ans:
-                ans.append([x, y])
-    if tm == td == sm == sd == 0:
-        meet = 4
+    E = ((y2 - y1) * x1) + ((x1 - x2) * y1)
+    F = ((y4 - y3) * x3) + ((x3 - x4) * y3)
+    DE = ((y2 - y1) * (x3 - x4)) - ((x1 - x2) * (y4 - y3))
+    if not DE:
+        return
+    else:
+        X = ((E * (x3 - x4)) - ((x1 - x2) * F)) / DE
+        Y = (((y2 - y1) * F) - (E * (y4 - y3))) / DE
+        if x1 <= X <= x2 and x3 <= X <= x4 and y1 <= Y <= y2 and y3 <= Y <= y4 and [X, Y] not in ans:
+            ans.append([X, Y])
 
 T = int(input().replace('혻', ''))
 for test_case in range(T):
@@ -29,5 +55,6 @@ for test_case in range(T):
     rectangleLine = [[xmin, ymin, xmax, ymin], [xmin, ymin, xmin, ymax], [xmin, ymax, xmax, ymax], [xmax, ymin, xmax, ymax]]
 
     for i in rectangleLine:
-        intersection(x1, y1, x2, y2, i[0], i[1], i[2], i[3])
+        intersection(i[0], i[1], i[2], i[3])
+    print(ans)
     print("#{} {}".format(test_case + 1, meet))
