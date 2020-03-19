b33239d0951553cc66380c7f14c586eb022a5076
hongyong3 <chy66822495@gmail.com>
2020-03-19 23:13:18 +0900

    D418 | 20200319 PM | Algorithm

--- Algorithm/Swea/D2_4880.py
+++ Algorithm/Swea/D2_4880.py
@@ -1,11 +1,87 @@
 import sys
 sys.stdin = open("D2_4880_input.txt", "r")
 
+# 이전 풀이
+# def tournament(data, x, y):
+#     a = data[x]
+#     b = data[y]
+#     if a == b:
+#         return x
+#     elif a == 1 and  b == 2:
+#         return y
+#     elif a == 1 and b == 3:
+#         return x
+#     elif a == 2 and b == 1:
+#         return x
+#     elif a == 2 and b == 3:
+#         return y
+#     elif a == 3 and b == 1:
+#         return y
+#     elif a == 3 and b == 2:
+#         return x
+#
+# def game(data, start, end):
+#     if end-start >=1:
+#         middle = int((start+end) // 2)
+#         return tournament(data, game(data, start, middle), game(data, middle+1, end))
+#     elif start == end:
+#         return start
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = list(map(int, input().split()))
+#     print(f"#{test_case+1} {game(data, 0, N-1) + 1}")
+
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = [[x + 1, y] for x, y in enumerate(map(int, input().split()))]
+#
+#     while len(data) > 1:
+#         for _ in range(0, len(data), 2):
+#             p1, p2 = data.pop(0), data.pop(0)
+#             if (p1[1] == 2 and p2[1] == 1) or (p1[1] == 3 and p2[1] == 2) or (p1[1] == 1 and p2[1] == 3) or (p1[0] < p2[0] and p1[1] == p2[1]):
+#                 data.append(p1)
+#             else:
+#                 data.append(p2)
+#     print("#{} {}".format(test_case + 1, data[0][0]))
+
+
+# 실패
+
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = [[x + 1, y] for x, y in enumerate(map(int, input().split()))]
+#     print(data)
+#
+#     while len(data) > 1:
+#         for _ in range(0, len(data), 2):
+#             p1, p2 = data.pop(0), data.pop(0)
+#
+#             if (p1[1] - p2[1] == 1) or (p1[1] - p2[1] == - 2) or (p1[0] < p2[0] and p1[1] == p2[1]):
+#                 data.append(p1)
+#             elif (p1[1] - p2[1] == - 1) or (p1[1] - p2[1] == 2):
+#                 data.append(p2)
+#             # else:
+#             #     data.append(p2)
+#     print("#{} {}".format(test_case + 1, data[0][0]))
+
+def match(x, y):
+    if data[x - 1] - data[y - 1] == 1 or data[x - 1] - data[y - 1] == - 2 or data[x - 1] == data[y - 1]:
+        return x
+    return y
+
+def tournament(start, end):
+    if start == end:
+        return start
+    first = tournament(start, (start + end) // 2)
+    second = tournament((start + end) // 2 + 1, end)
+    return match(first, second)
+
 T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
-    people = {}
-    for i in range(N):
-        people[i + 1] = data[i]
-    print(people)
+    print("#{} {}".format(test_case + 1, tournament(1, N)))
---
+++ Algorithm/Swea/D2_4881.py
@@ -0,0 +1,53 @@
+import sys
+sys.stdin = open("D2_4881_input.txt", "r")
+
+# 이전 풀이
+# def MinSum(k):
+#     global min, sum, N
+#     if k == N:
+#         if min > sum:
+#             min = sum
+#     for i in range(N):
+#         if visit[i] == 0:
+#             if sum > min:
+#                 continue
+#             else:
+#                 visit[i] = 1
+#                 sum += data[i][k]
+#                 MinSum(k+1)
+#                 visit[i] = 0
+#                 sum -= data[i][k]
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = [list(map(int, input().split())) for _ in range(N)]
+#     visit = [0]*N
+#     min = 10**10
+#     sum = 0
+#     MinSum(0)
+#
+#     print('#{} {}'.format(test_case+1, min))
+
+def arr(n):
+    global ans, subAns, visited
+    if n == N:
+        if ans > subAns:
+            ans = subAns
+
+    for i in range(N):
+        if not visited[i]:
+            if subAns < ans:
+                visited[i] = 1
+                subAns += data[i][n]
+                arr(n + 1)
+                visited[i] = 0
+                subAns -= data[i][n]
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    data = [list(map(int, input().split())) for _ in range(N)]
+    ans, subAns = float('inf'), 0
+    visited = [0] * N
+    arr(0)
+    print("#{} {}".format(test_case + 1, ans))
