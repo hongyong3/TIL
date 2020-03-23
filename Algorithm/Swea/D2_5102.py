49cd97ab4e3f88094e4da51886c896677482d6a3
hongyong3 <chy66822495@gmail.com>
2020-03-23 22:41:51 +0900

    D422 | 20200323 PM | Algorithm

--- Algorithm/Swea/D2_5102.py
+++ Algorithm/Swea/D2_5102.py
@@ -1,10 +1,41 @@
 import sys
 sys.stdin = open("D2_5102_input.txt", "r")
 
-def bfs(graph, s):
+# 이전 풀이
+# def bfs(data, v):  # 1에서 가장 멀리 있는 정점을 찾으시오.
+#     global G
+#     queue=[]
+#
+#     queue.append(v)  #enQueue하면서 방문처리
+#     visited[v] = 1
+#
+#     while len(queue) != 0:
+#         t = queue.pop(0)
+#         for w in range(1, V+1):
+#             if data[t][w] == 1 and visited[w] == 0:
+#                 queue.append(w)
+#                 visited[w] = visited[t] + 1
+#     result = 0
+#     if visited[G]:
+#         result = visited[G]-1
+#     print(f"#{test_case} {result}")
+#
+# T = int(input())
+# for test_case in range(1, T+1):
+#     V, E = map(int, input().split())
+#     data = [[0 for _ in range(V+1)] for _ in range(V+1)]
+#     visited = [0 for _ in range(V + 1)]
+#     for C in range(E):
+#         i, j = map(int, input().split())
+#         data[i][j] = 1
+#         data[j][i] = 1
+#     S, G = map(int, input().split())
+#     bfs(data, S)
+
+def bfs(graph, S):
     q = []
-    q.append(s)
-    visited[s] = 1
+    q.append(S)
+    visited[S] = 1
 
     while q:
         s = q.pop(0)
@@ -15,7 +46,7 @@ def bfs(graph, s):
     ans = 0
     if visited[G]:
         ans = visited[G] - 1
-    print("#{} {}".format(test_case + 1, ans))
+    return ans
 
 
 T = int(input())
@@ -27,4 +58,4 @@ for test_case in range(T):
         i, j = map(int, input().split())
         graph[i][j], graph[j][i] = 1, 1
     S, G = map(int, input().split())
-    bfs(graph, S)
+    print("#{} {}".format(test_case + 1, bfs(graph, S)))
--- Algorithm/Swea/D4_3501.py
+++ Algorithm/Swea/D4_3501.py
@@ -1,24 +1,33 @@
 import sys
 sys.stdin = open("D4_3501_input.txt", "r")
 
+# T = int(input())
+# for test_case in range(T):
+#    p, q = map(int, input().split())
+#    ans = '0.'
+#    if p / q == p // q:
+#        print("#{} {}".format(test_case + 1, p // q))
+#    else:
+#        if len(str(float(p) / float(q))) <= 4:
+#            print("#{} {}".format(test_case + 1, p / q))
+#        else:
+#            circulating = str("%.50f" % (p / q))[2:]
+#            for i in range(len(circulating)):
+#                j = i + 1
+#                while j < len(circulating):
+#                    if circulating[i : j] == circulating[j : 2 * j]:
+#                        ans += '(' + circulating[i : j] + ')'
+#                        break
+#                    else:
+#                        j += 1
+#                break
+#            print("#{} {}".format(test_case + 1, ans))
+
+from decimal import *
+getcontext().prec = 100
+
 T = int(input())
 for test_case in range(T):
-   p, q = map(int, input().split())
-   ans = '0.'
-   if p / q == p // q:
-       print("#{} {}".format(test_case + 1, p // q))
-   else:
-       if len(str(float(p) / float(q))) <= 4:
-           print("#{} {}".format(test_case + 1, p / q))
-       else:
-           circulating = str("%.50f" % (p / q))[2:]
-           for i in range(len(circulating)):
-               j = i + 1
-               while j < len(circulating):
-                   if circulating[i : j] == circulating[j : 2 * j]:
-                       ans += '(' + circulating[i : j] + ')'
-                       break
-                   else:
-                       j += 1
-               break
-           print("#{} {}".format(test_case + 1, ans))
+    p, q = map(int, input().split())
+    a = Decimal(p) / Decimal(q)
+    print("#{} {}".format(test_case + 1, a))
