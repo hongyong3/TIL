2a41f460185f98e36d90957dec9c6beaadcef3db
hongyong3 <chy66822495@gmail.com>
2020-03-31 20:37:34 +0900

    D430 | 20200331 PM | Algorithm

--- Algorithm/Swea/D3_1225.py
+++ Algorithm/Swea/D3_1225.py
@@ -1,23 +1,28 @@
 import sys
 sys.stdin = open("D3_1225_input.txt", "r")
 
-def solve(data):
-    while True:
-        for i in range(1, 6):
-            data[0] = data[0] - i
-            if data[0] < 1:
-                data[0] = 0
-                data.append(data.pop(0))
-                return
-            data.append(data.pop(0))
+# 이전 풀이
+# def solve(data):
+#     while True:
+#         for i in range(1, 6):
+#             data[0] = data[0] - i
+#             if data[0] < 1:
+#                 data[0] = 0
+#                 data.append(data.pop(0))
+#                 return
+#             data.append(data.pop(0))
+#
+# for test_case in range(10):
+#     N = int(input())
+#     data = list(map(int, input().split()))
+#     ans = 0
+#     if min(data) > 15:
+#         ans = min(data) // 15
+#     for i in range(len(data)):
+#         data[i] = data[i] - ((ans - 1) * 15)
+#     solve(data)
+#     print("#{}".format(test_case + 1), *data)
 
 for test_case in range(10):
     N = int(input())
     data = list(map(int, input().split()))
-    ans = 0
-    if min(data) > 15:
-        ans = min(data) // 15
-    for i in range(len(data)):
-        data[i] = data[i] - ((ans - 1) * 15)
-    solve(data)
-    print("#{}".format(test_case + 1), *data)
