6c3d565d14da4e48e61c04ce5276e50a937ea41d
hongyong3 <chy66822495@gmail.com>
2020-04-07 21:51:21 +0900

    D437 | 20200407 PM | Algorithm

--- Algorithm/Swea/D3_5204.py
+++ Algorithm/Swea/D3_5204.py
@@ -6,6 +6,3 @@ for _test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
     data1, data2 = data[:N // 2], data[N // 2:]
-    print(data)
-    print(data1, data2)
-    print(sorted(data1), sorted(data2))
