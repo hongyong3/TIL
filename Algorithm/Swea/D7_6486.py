ea56981db192ec4172fd868295227a007e27552e
hongyong3 <chy66822495@gmail.com>
2020-04-15 19:23:48 +0900

    D445 | 20200415 PM | Algorithm

--- Algorithm/Swea/D7_6486.py
+++ Algorithm/Swea/D7_6486.py
@@ -52,32 +52,101 @@ sys.stdin = open("D7_6486_input.txt", "r")
 #     print("#{} {}".format(test_case + 1, ans))
 
 # Runtime Error (17 / 100)
-def powerset(n):
-    if n == N:
-        num = 0
-        for i in range(n):
-            if checked[i]:
-                num += data[i]
-        if num in arr:
-            arr.remove(num)
+# def powerset(n):
+#     if n == N:
+#         num = 0
+#         for i in range(n):
+#             if checked[i]:
+#                 num += data[i]
+#         if num in arr:
+#             arr.remove(num)
+#         else:
+#             arr.append(num)
+#         return
+#
+#     checked[n] = 0
+#     powerset(n + 1)
+#     checked[n] = 1
+#     powerset(n + 1)
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = list(map(int, input().split()))
+#     arr = []
+#     checked = [0] * N
+#     powerset(0)
+#     ans = arr.pop()
+#
+#     for i in arr:
+#         ans ^= i
+#     print("#{} {}".format(test_case + 1, ans))
+
+def sort(b, a, l):
+    j, k = 0, 0
+    for i in range(a):
+        if not b[i] >> l & 1:
+            k += 1
+
+    for i in range(a):
+        if b[i] >> l & 1:
+            k += 1
+            t[k] = b[i]
+
         else:
-            arr.append(num)
-        return
+            j += 1
+            t[j] = b[i]
 
-    checked[n] = 0
-    powerset(n + 1)
-    checked[n] = 1
-    powerset(n + 1)
+    for i in range(a):
+        b[i] = t[i]
 
+
+a = [0] * 30
+c = [0] * 32768
+d = [0] * 32768
+t = [0] * 32768
 T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
-    arr = []
-    checked = [0] * N
-    powerset(0)
-    ans = arr.pop()
 
-    for i in arr:
-        ans ^= i
-    print("#{} {}".format(test_case + 1, ans))
+    for i in range(N):
+        a[i] = data[i]
+
+    n = N >> 1
+    M = N - n
+    T, R = 0, 0
+
+    for i in range(1 << n):
+        c[i] = 0
+        for j in range(n):
+            if i >> j & 1:
+                c[i] += a[j]
+
+    for i in range(1 << M):
+        d[i] = 0
+        for j in range(M):
+            if i >> j & 1:
+                d[i] += a[j + n]
+
+    n = 1 << n
+    M = 1 << M
+
+    for i in range(35):
+        T = (2 << i) - 1
+        sort(c, n, i)
+        sort(d, M, i)
+        w = x = y = z = 0
+
+        for j in range(n - 1, - 1, - 1):
+            while x < M and ((c[j] & T) + (d[x] & T) >> i) < 1:
+                x += 1
+            while y < M and ((c[j] & T) + (d[y] & T) >> i) < 2:
+                y += 1
+            while z < M and ((c[j] & T) + (d[z] & T) >> i) < 3:
+                z += 1
+            w += y - x + M - z
+
+        if w & 1:
+            R |= 1 << i
+    print(R)
