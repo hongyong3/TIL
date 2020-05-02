d35aadd24b6f2d4b636217ca0387c3490c821848
hongyong3 <chy66822495@gmail.com>
2020-05-02 19:03:14 +0900

    D461 | 20200502 PM | Algorithm

--- Algorithm/Swea/D5_4411.py
+++ Algorithm/Swea/D5_4411.py
@@ -3,7 +3,7 @@ sys.stdin = open("D5_4411_input.txt", "r")
 
 # 요세푸스 점화식
 T = int(input())
-for test_case in range(T):
+for test_case in range(1):
     N, K = map(int, input().split())
     ans = 0
     for i in range(1, N + 1):
