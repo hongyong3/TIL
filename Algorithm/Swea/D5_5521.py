2608ae114e534e3cf6c6b2f543858d798aa84d6a
hongyong3 <chy66822495@gmail.com>
2020-05-11 17:49:08 +0900

    D469 | 20200511 PM | Algorithm

--- Algorithm/Swea/D5_5521.py
+++ Algorithm/Swea/D5_5521.py
@@ -1,14 +1,30 @@
 import sys
 sys.stdin = open("D5_5521_input.txt", "r")
 
+def bfs():
+    global ans
+    q = []
+    for i in range(1, N + 1):
+        if data[1][i]:
+            ans += 1
+            visited[i] = 1
+            q.append(i)
+    while q:
+        n = q.pop()
+        for i in range(1, N + 1):
+            if data[n][i] and not visited[i]:
+                ans += 1
+    return ans
+
 T = int(input().replace('혻', ''))
-for test_case in range(1):
+for test_case in range(T):
     N, M = map(int, input().replace('혻', '').split())
     data = [[0] * (N + 1) for _ in range(N + 1)]
     visited = [0] * (N + 1)
     visited[1] = 1
     ans = 0
+
     for _ in range(M):
         a, b = map(int, input().replace('혻', '').split())
         data[a][b], data[b][a] = 1, 1
-    
+    print("#{} {}".format(test_case + 1, bfs()))
--- Algorithm/Swea/D5_7585.py
+++ Algorithm/Swea/D5_7585.py
@@ -1,17 +1,39 @@
 import sys
 sys.stdin = open("D5_7585_input.txt", "r")
 
-# input시 Runtime Error...
+
+def solve(n, p):
+    temp = 1
+    while p:
+        if p & 1:
+            temp = (temp * n) % K
+        n = (n * n) % K
+        p >>= 1
+    return temp
+
+MOD = 1000000007
 T = int(input())
 for test_case in range(T):
     A, B, N, K = map(int, input().split())
-    visited = list(range(1, N + 1))
-    count = 0
-    for i in range(1, N + 1):
-        a = pow(i, A)
-        for j in range(1, N + 1):
-            if i != j:
-                b = pow(j, B)
-                if not (a + b) % K:
-                    count += 1
-    print("#{} {}".format(test_case + 1, count))
+    iarr = [0] * 100001
+    jarr = [0] * 100001
+    m = 0
+    for i in range(1, min(N, K) + 1):
+        c = N // K
+        if i <= (N % K):
+            c += 1
+        iA, jB = solve(i, A) % K, solve(i, B) % K
+
+        iarr[iA] += c
+        jarr[jB] += c
+        iarr[iA] %= MOD
+        jarr[jB] %= MOD
+
+        if ((iA + jB) % K) == 0:
+            m = (m + c) % MOD
+
+    ans = (iarr[0] * jarr[0]) % MOD
+
+    for i in range(1, K):
+        ans = (ans + (iarr[i] * jarr[K - i]) % MOD) % MOD
+    print("#{} {}".format(test_case + 1, (ans - m + MOD) % MOD))
