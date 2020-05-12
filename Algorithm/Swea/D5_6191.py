35f88dc792c09ed202c94346cabd378a6959566d
hongyong3 <chy66822495@gmail.com>
2020-05-12 18:43:06 +0900

    D470 | 20200512 PM | Algorithm

---
+++ Algorithm/Swea/D5_6191.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("D5_6191_input.txt", "r")
+
+    # 점화식 fn = 1 + N * (N - 1) // 2
+# if K > f(N):
+#     ans = ')('
+
+
+
+arr = [0] * 44722
+arr[1] = 1
+for i in range(2, 44722):
+    arr[i] = arr[i - 1] + i
+
+T = int(input())
+for test_case in range(T):
+    N, K = map(int, input().split())
+    fn = 1 + N * (N - 1) // 2
+
+    if K > fn:
+        ans = ')('
+    else:
+        e = 44722
+        for i in range(e):
+            if N <= arr[i]:
+                e = i
+                break
+        s = arr[e] - N
+        ans = ')' * (e - s) + '(' + ')' * s + '(' * (e - 1)
+
+    print(ans)
--- Algorithm/Swea/D5_7585.py
+++ Algorithm/Swea/D5_7585.py
@@ -1,7 +1,6 @@
 import sys
 sys.stdin = open("D5_7585_input.txt", "r")
 
-
 def solve(n, p):
     temp = 1
     while p:
@@ -9,31 +8,29 @@ def solve(n, p):
             temp = (temp * n) % K
         n = (n * n) % K
         p >>= 1
-    return temp
+    return temp % K
 
 MOD = 1000000007
 T = int(input())
 for test_case in range(T):
     A, B, N, K = map(int, input().split())
-    iarr = [0] * 100001
-    jarr = [0] * 100001
+    iarr = [0] * K
+    jarr = [0] * K
     m = 0
     for i in range(1, min(N, K) + 1):
         c = N // K
         if i <= (N % K):
             c += 1
-        iA, jB = solve(i, A) % K, solve(i, B) % K
+        a, b = solve(i, A), solve(i, B)
 
-        iarr[iA] += c
-        jarr[jB] += c
-        iarr[iA] %= MOD
-        jarr[jB] %= MOD
+        iarr[a] = (iarr[a] + c) % MOD
+        jarr[b] = (jarr[b] + c) % MOD
 
-        if ((iA + jB) % K) == 0:
+        if not (a + b) % K:
             m = (m + c) % MOD
 
-    ans = (iarr[0] * jarr[0]) % MOD
+    ans = iarr[0] * jarr[0] % MOD
 
     for i in range(1, K):
-        ans = (ans + (iarr[i] * jarr[K - i]) % MOD) % MOD
-    print("#{} {}".format(test_case + 1, (ans - m + MOD) % MOD))
+        ans += iarr[i] * jarr[K - i] % MOD
+    print("#{} {}".format(test_case + 1, (ans - m) % MOD))
