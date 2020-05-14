7bc4a19743b5c69a36120d6c74696e3587f08bf4
hongyong3 <chy66822495@gmail.com>
2020-05-14 19:33:17 +0900

    D472 | 20200514 PM | Algorithm

--- Algorithm/Swea/D5_5521.py
+++ Algorithm/Swea/D5_5521.py
@@ -1,41 +1,24 @@
 import sys
 sys.stdin = open("D5_5521_input.txt", "r")
 
-# def bfs():
-#     global ans
-#     q = []
-#     for i in range(1, N + 1):
-#         if data[1][i]:
-#             ans += 1
-#             visited[i] = 1
-#             q.append(i)
-#     while q:
-#         n = q.pop()
-#         for i in range(1, N + 1):
-#             if data[n][i] and not visited[i]:
-#                 ans += 1
-#     return ans
-
 T = int(input().replace('혻', ''))
 for test_case in range(T):
     N, M = map(int, input().replace('혻', '').split())
-    data = [[0] * (N + 1) for _ in range(N + 1)]
+    data = [[] for _ in range(N + 1)]
     visited = [0] * (N + 1)
     visited[1] = 1
-    ans = 0
-    check = []
     for _ in range(M):
         a, b = map(int, input().replace('혻', '').split())
-        data[a][b], data[b][a] = 1, 1
+        data[a].append(b)
+        data[b].append(a)
         if a == 1:
             visited[b] = 1
-            ans += 1
-            check.append(b)
 
-    for i in check:
-        for j in range(1, N + 1):
-            if not visited[data[i][j]]:
+    ans = len(data[1])
+    for i in range(len(data[1])):
+        for j in range(len(data[data[1][i]])):
+            if not visited[data[data[1][i]][j]]:
                 ans += 1
-                visited[data[i][j]] = 1
+                visited[data[data[1][i]][j]] = 1
 
     print("#{} {}".format(test_case + 1, ans))
