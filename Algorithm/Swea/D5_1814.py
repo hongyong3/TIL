c72820167728a87538c0abc1d359d388baa97bb5
hongyong3 <chy66822495@gmail.com>
2020-05-29 22:55:39 +0900

    D487 | 20200529 PM | Algorithm

--- Algorithm/Swea/D5_1814.py
+++ Algorithm/Swea/D5_1814.py
@@ -4,11 +4,11 @@ sys.stdin = open("D5_1814_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    # arr = [[0] * (N + 1) for _ in range(N + 1)]
     line = list(map(int, input().split()))
     row = list(map(int, input().split()))
     ans = 'Yes'
     if (line.count(N) and line.count(0)) or (line.count(0) and line.count(N)):
         ans = 'No'
-    # ㄷㄷ;; 방법을 생각하자..
+    else:
+        pass
     print("#{} {}".format(test_case + 1, ans))
