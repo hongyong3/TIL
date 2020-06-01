797062e82c6c961375629ec27048db471446776f
hongyong3 <chy66822495@gmail.com>
2020-06-01 18:32:42 +0900

    D490 | 20200601 PM | Algorithm

--- Algorithm/Swea/D5_5170.py
+++ Algorithm/Swea/D5_5170.py
@@ -1,17 +1,50 @@
 import sys
 sys.stdin = open("D5_5170_input.txt", "r")
 
+# 방법1 list
+# def solve(p1, p2):
+#     if p1[0] == p2[0]:
+#         return 9999
+#     return (p2[1] - p1[1]) / (p2[0] - p1[0])
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = sorted([list(map(int, input().split())) for _ in range(N)])
+#     arr = []
+#     for i in range(N):
+#         for j in range(i + 1, N):
+#             arr.append(solve(data[i], data[j]))
+#     print("#{} {}".format(test_case + 1, len(set(arr))))
+
+# 방법2 set
+# def solve(p1, p2):
+#     if p1[0] == p2[0]:
+#         return 9999
+#     return (p2[1] - p1[1]) / (p2[0] - p1[0])
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     data = sorted([list(map(int, input().split())) for _ in range(N)])
+#     arr = set()
+#     for i in range(N):
+#         for j in range(i + 1, N):
+#             arr.add(solve(data[i], data[j]))
+#     print("#{} {}".format(test_case + 1, len(arr)))
+
+# 방법3 재귀없이
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    data = sorted([list(map(int, input().split())) for _ in range(N)])
-    arr = []
+    data = [list(map(int, input().split())) for _ in range(N)]
+    arr = set()
     chk = False
     for i in range(N):
         for j in range(i + 1, N):
-            if (data[j][1] - data[i][1]) == 0 or (data[j][0] - data[i][0]) == 0:
+            if data[i][0] == data[j][0]:
                 chk = True
+                continue
             else:
-                x = (data[j][1] - data[i][1]) / (data[j][0] - data[i][0])
-                arr.append(x)
-    print("#{} {}".format(test_case + 1, len(set(arr)) + chk))
+                arr.add((data[j][1] - data[i][1]) / (data[j][0] - data[i][0]))
+    print("#{} {}".format(test_case + 1, len(arr) + chk))
