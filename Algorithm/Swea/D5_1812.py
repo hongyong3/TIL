044a21608b5061821dadcc3a82c819d33f47ea55
hongyong3 <chy66822495@gmail.com>
2020-06-11 02:28:11 +0900

    D500 | 20200611 AM | Algorithm

--- Algorithm/Swea/D5_1812.py
+++ Algorithm/Swea/D5_1812.py
@@ -2,6 +2,11 @@ import sys
 sys.stdin = open("D5_1812_input.txt", "r")
 
 T = int(input())
-for test_case in range(T):
+for test_case in range(5):
     N, M = map(int, input().split())
-    data = list(map(int, input().split()))
+    data = sorted((list(map(int, input().split()))), reverse= True)
+    ans = 0
+    tile = []
+    for i in data:
+        tile.append(pow(2, i))
+    
