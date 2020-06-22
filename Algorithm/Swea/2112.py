32960e4f5342f7d0937117932e6f9caf396f1879
hongyong3 <chy66822495@gmail.com>
2020-06-22 22:04:30 +0900

    D511 | 20200622 PM | Algorithm

--- Algorithm/Swea/2112.py
+++ Algorithm/Swea/2112.py
@@ -3,17 +3,34 @@ sys.stdin = open("2112_input.txt", "r")
 
 def solve(curD, chemicalCnt, prevContinuum, prevMaxContinuum):
     global minChemicalCnt
+
     if chemicalCnt >= minChemicalCnt:
         return
+
     if curD == D:
         chk = True
         for i in range(W):
-            if prevContinuum[i] < K:
+            if prevMaxContinuum[i] < K:
                 chk = False
                 break
 
         if chk and chemicalCnt < minChemicalCnt:
             minChemicalCnt = chemicalCnt
+        return
+
+    for i in range(2, - 1, - 1):
+        chemical[curD] = i
+        for j in range(W):
+            cur = film[curD][j] if chemical[curD] == 2 else chemical[curD]
+            prev = film[curD - 1][j] if chemical[curD - 1] == 2 else chemical[curD - 1]
+            continuum[j] = prevContinuum[j] + 1 if cur == prev else 1
+            if continuum[j] > prevMaxContinuum[j]:
+                maxContinuum[j] = continuum[j]
+            else:
+                maxContinuum[j] = prevMaxContinuum[j]
+
+        a = 0 if i == 2 else 1
+        solve(curD + 1, chemicalCnt + a, continuum, maxContinuum)
 
 
 T = int(input())
