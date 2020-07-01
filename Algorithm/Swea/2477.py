b862a645d4df51369b6728f45fc7797d0251cfaf
hongyong3 <chy66822495@gmail.com>
2020-07-01 22:10:55 +0900

    D520 | 20200701 PM | Algorithm

---
+++ Algorithm/Swea/2477.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("2477_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N, M, K, A, B = map(int, input().split())
+    adata = list(map(int, input().split()))
+    bdata = list(map(int, input().split()))
+    tdata = list(map(int, input().split()))
