a67e0dccd06ce2f02d63a5e205f4b0bc9ccce553
hongyong3 <chy66822495@gmail.com>
2020-07-11 21:52:34 +0900

    D530 | 20200711 PM | Algorithm

--- Algorithm/Swea/5644.py
+++ Algorithm/Swea/5644.py
@@ -1,10 +1,11 @@
 import sys
 sys.stdin = open("5644_input.txt", "r")
 
-dx = [0, - 1, 0, 1, 0]  # 무상우하좌
-dy = [0, 0, - 1, 0, 1]
+dy = [0, - 1, 0, 1, 0]  # 무상우하좌
+dx = [0, 0, - 1, 0, 1]
 
 def solve(ax, ay, bx, by):
+    global ans
     ac = []
     bc = []
 
@@ -15,6 +16,33 @@ def solve(ax, ay, bx, by):
         if abs(bx - X) + abs(by - Y) <= C:
             bc.append((i, P))
 
+    ac.sort(key = lambda x : x[1], reverse = True)
+    bc.sort(key = lambda x : x[1], reverse = True)
+
+    temp1 = 0
+    uu1 = 0
+    for idx, P in ac:
+        uu1 = idx
+        temp1 += P
+        break
+    for idx, P in bc:
+        if idx != uu1:
+            temp1 += P
+            break
+
+    temp2 = 0
+    uu2 = 0
+    for idx, P in bc:
+        uu2 = idx
+        temp2 += P
+        break
+    for idx, P in ac:
+        if idx != uu2:
+            temp2 += P
+            break
+
+    ans += max(temp1, temp2)
+
 T = int(input())
 for test_case in range(T):
     # M : 이동 시간; A : BC의 개수;
@@ -24,10 +52,13 @@ for test_case in range(T):
     a2 = list(map(int, input().split()))
     # (X, Y) : 좌표; C : 충전 범위; P : 처리량;
     AP = [list(map(int, input().split())) for _ in range(A)]
-    arr = [[0] * 10 for _ in range(10)]
 
     Ax, Ay = 1, 1
     Bx, By = 10, 10
+    ans = 0
+    solve(Ax, Ay, Bx, By)
     for i in range(M):
         Ax, Ay = Ax + dx[a1[i]], Ay + dy[a1[i]]
         Bx, By = Bx + dx[a2[i]], By + dy[a2[i]]
+        solve(Ax, Ay, Bx, By)
+    print(ans)
