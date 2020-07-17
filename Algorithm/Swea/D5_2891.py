276520418ae278437f51edabece790c69f667ed4
hongyong3 <chy66822495@gmail.com>
2020-07-17 19:35:53 +0900

    D536 | 20200717 PM | Algorithm

---
+++ Algorithm/Swea/D5_2891.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open("D5_2891_input.txt", "r")
+
+num = '123456789'
+T = int(input())
+for test_case in range(1):
+    garo = [[0] * 10 for _ in range(7)]
+    garoNum = [[0] * 10 for _ in range(7)]
+    sero = [[[0] * 10 for _ in range(7)] for _ in range(2)]
+    for i in range(1, 7):
+        data = list(map(str, input().split()))
+        print(*data)
+        k = 1
+        for j in range(6):
+            if len(data[j]) == 3:
+                if data[j][0] == '-':
+                    k += 1
+                else:
+                    garo[i][k] = 1
+                    garoNum[i][int(data[j][0])] = 1
+                    k += 1
+                if data[j][2] == '-':
+                    k += 1
+                else:
+                    garo[i][k] = 1
+                    garoNum[i][int(data[j][2])] = 1
+                    k += 1
+            else:
+                if data[j] == '-':
+                    k += 1
+                else:
+                    garo[i][k] = 1
+                    garoNum[i][int(data[j])] = 1
+                    k += 1
+    print()
+    for i in garo:
+        print(*i)
+    print()
+    for i in garoNum:
+        print(*i)
--- Algorithm/Swea/D6_1795.py
+++ Algorithm/Swea/D6_1795.py
@@ -1,30 +1,87 @@
 import sys
 sys.stdin = open("D6_1795_input.txt", "r")
 
-def djikstra():
-    dist[X] = 0
+# djikstra? O
+# or cycle -> X
+
+# def djikstra():
+#     time1[X] = time2[X] = 0
+#     q1, q2 = [], []
+#     q1.append([X, 0])
+#     q2.append([X, 0])
+#
+#     while q1:
+#         x, c = q1.pop()
+#         if time1[x] < c:
+#             continue
+#         for i in graph1[x]:
+#             nx = i[0]
+#             nt = i[1] + c
+#             if time1[nx] > nt:
+#                 time1[nx] = nt
+#                 q1.append([nx, nt])
+#
+#     while q2:
+#         y, c = q2.pop()
+#         if time2[y] < c:
+#             continue
+#         for i in graph2[y]:
+#             ny = i[0]
+#             nt = i[1] + c
+#             if time2[ny] > nt:
+#                 time2[ny] = nt
+#                 q2.append([ny, nt])
+#
+#
+# T = int(input())
+# for test_case in range(T):
+#     N, M, X = map(int, input().split())
+#     graph1 = [[] for _ in range(N + 1)]
+#     graph2 = [[] for _ in range(N + 1)]
+#     time1 = [float('inf')] * (N + 1)
+#     time2 = [float('inf')] * (N + 1)
+#     ans = 0
+#     for _ in range(M):
+#         x, y, c = map(int, input().split())  # x -> y, time
+#         graph1[x].append([y, c])  # add y and time in graph's x index
+#         graph2[y].append([x, c])  # add x and time in graph's y index
+#     djikstra()
+#
+#     for i in range(1, N + 1):
+#         ans = max(ans, time1[i] + time2[i])
+#     print("#{} {}".format(test_case + 1, ans))
+
+def djikstra(n):
+    if n > 1:
+        return
     q = []
     q.append([X, 0])
+
     while q:
-        now, time = q.pop()
-        if dist[now] > time:
+        x, c = q.pop()
+        if time[n][x] < c:
             continue
-        for i in range(len(graph[now])):
-            there = graph[now][i][0]
-            nextDist = graph[now][i][1] + time
-            if dist[there] < nextDist:
-                dist[there] = nextDist
-                q.append([there, nextDist])
+        for i in graph[n][x]:
+            nx = i[0]
+            nt = i[1] + c
+            if time[n][nx] > nt:
+                time[n][nx] = nt
+                q.append([nx, nt])
+    djikstra(n + 1)
+
 
 T = int(input())
 for test_case in range(T):
     N, M, X = map(int, input().split())
-    graph = [[] for _ in range(N + 1)]
-    dist = [float('inf')] * (N + 1)
-    ans = - 1
+    graph = [[[] for _ in range(N + 1)] for _ in range(2)]
+    time = [[float('inf')] * (N + 1) for _ in range(2)]
+    time[0][X] = time[1][X] = 0
     for _ in range(M):
-        x, y, c = map(int, input().split())
-        graph[x].append([y, c])
-    print(graph)
-    djikstra()
-    print(dist)
+        x, y, c = map(int, input().split()) # x -> y, time
+        graph[0][x].append([y, c])    # add y and time in graph's x index
+        graph[1][y].append([x, c])    # add x and time in graph's y index
+    djikstra(0)
+    ans = 0
+    for i in range(1, N + 1):
+        ans = max(ans, time[0][i] + time[1][i])
+    print("#{} {}".format(test_case + 1, ans))
