0270a48e33db102d97a67a9f44445ca6bd1544ca
hongyong3 <chy66822495@gmail.com>
2020-07-24 19:16:47 +0900

    D542 | 20200724 PM | Algorithm

--- Algorithm/Swea/D5_10204.py
+++ Algorithm/Swea/D5_10204.py
@@ -1,20 +1,14 @@
 import sys
 sys.stdin = open("D5_10204_input.txt", "r")
 
+# 6 / 13 rumtime error..
+'''
+sort를 어떻게 해야하는가...
+'''
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    data = sorted([list(map(int, input().split())) for _ in range(N)], key = lambda x : (- x[0], - x[1]))
-    ans = [data[i][0] if not i % 2 else - data[i][1] for i in range(N)]
-    print("#{} {}".format(test_case + 1, sum(ans)))
-    # print(ans)
-    # sortedData = sorted(data, key = lambda x : (- x[0], - x[1]))
-    # ans = 0
-    # print(sortedData)
-    # for i in range(N):
-    #     if not i % 2:
-    #         ans += sortedData[i][0]
-    #     else:
-    #         ans -= sortedData[i][1]
-    # print("#{} {}".format(test_case + 1, ans1 - ans2))
-    # print("#{} {}".format(test_case + 1, ans))
+    # data = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: (- x[0], - x[1]))
+    data = sorted([list(map(int, input().split())) for _ in range(N)], reverse = True)
+    ans = sum([data[i][0] if not i % 2 else - data[i][1] for i in range(N)])
+    print("#{} {}".format(test_case + 1, ans))
