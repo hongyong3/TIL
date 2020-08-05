298befb8b29743903412f091ce63c538c95b443e
hongyong3 <chy66822495@gmail.com>
2020-08-05 18:45:48 +0900

    D554 | 20200805 PM | Algorithm

--- Algorithm/Swea/4014.py
+++ Algorithm/Swea/4014.py
@@ -1,12 +1,47 @@
 import sys
 sys.stdin = open("4014_input.txt", "r")
 
+# 높이 차 2 이상 : no
+# 높이 차 1
+# 갯수 < X이면 ans += 1
+
+def solve(arr):
+    cnt = 1
+    value = arr[0]
+    for i in range(1, N):
+        if arr[i] < value:
+            if abs(value - arr[i]) > 1:
+                return 0
+            else:
+                if i + X > N:
+                    return 0
+                else:
+                    for j in range(i, i + X):
+                        if arr[j] != arr[i]:
+                            return 0
+                    cnt = - X + 1
+        elif arr[i] > value:
+            if arr[i] - value > 1:
+                return 0
+            else:
+                if cnt >= X:
+                    cnt += 1
+                else:
+                    return 0
+        else:
+            cnt += 1
+        value = arr[i]
+    return 1
+
+
 T = int(input())
-for test_case in range(1):
+for test_case in range(T):
     N, X = map(int, input().split())
     dataRow = [list(map(int, input().split())) for _ in range(N)]
     dataCol = [[element for element in t] for t in zip(*dataRow)]
     ans = 0
 
     for i in range(N):
-        
+        ans += solve(dataRow[i])
+        ans += solve(dataCol[i])
+    print("#{} {}".format(test_case + 1, ans))
