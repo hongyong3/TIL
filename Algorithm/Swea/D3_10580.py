879bf0216d6cf6c4966630fb2bad5b2fa7aff710
hongyong3 <chy66822495@gmail.com>
2020-08-17 17:28:22 +0900

    D565 | 20200817 PM | Algorithm

--- Algorithm/Swea/D3_10580.py
+++ Algorithm/Swea/D3_10580.py
@@ -4,23 +4,17 @@ sys.stdin = open("D3_10580_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    p, cp, c, cDot, chk = 0, 0, 0, set(), set() # 평행, 교차-평행O, 교차-평행X, 교차-평행 확인
-    for i in range(N):
-        a, b = map(int, input().split())
-        if a == b:  # 평행이면
-            p += 1
-        else:   # 교차이면
-            if a - b not in cDot:  # 평행X이면
-                cDot.add(a - b)
-                c += 1
-            else:   # 평행이면
-                if a - b not in chk:
-                    cp += 1
-                    chk.add(a - b)
+    data = sorted([list(map(int, input().split())) for _ in range(N)])
+    ans,cnt = 0, 0
 
-    # print(p, cp, c) # 평행, 교차평행, 교차
-    ans = p * cp
-    if c:
-        ans += (p * c) + c - 1
-    # ans = (p * c) + c - 1 + (p * cp)
-    print("#{} {}".format(test_case + 1, ans))
+    for i in range(N):
+        minVal, maxVal = min(data[i][0], data[i][1]), max(data[i][0], data[i][1])
+        for j in range(N):
+            if i == j:
+                continue
+            else:
+                if minVal <= data[j][0] <= maxVal and minVal <= data[j][1] <= maxVal:
+                    ans += 1
+                if (minVal == data[j][0] and maxVal == data[j][1]) or (maxVal == data[j][0] and minVal == data[j][1]):
+                    cnt += 1
+    print("#{} {}".format(test_case + 1, ans + cnt // 2))
