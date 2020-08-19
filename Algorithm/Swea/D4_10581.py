54930fcfaa8e3d54facda5e06d1fdd85e71bfd01
hongyong3 <chy66822495@gmail.com>
2020-08-19 16:42:17 +0900

    D567 | 20200819 PM | Algorithm

--- Algorithm/Swea/D4_10581.py
+++ Algorithm/Swea/D4_10581.py
@@ -8,15 +8,29 @@ sys.stdin = open("D4_10581_input.txt", "r")
 #         b -= mid // 2
 #
 
+# T = int(input())
+# for test_case in range(T):
+#     L, P, C = map(int, input().split())
+#     start = L + 1
+#     end = P // C if P / C == P // C else P // C + 1
+#     ans = 0
+#     while start < end:
+#         bitween = (end - start) // 2
+#         start += bitween
+#         end -= bitween
+#         ans += 1
+#     print(ans)
+
 T = int(input())
 for test_case in range(T):
     L, P, C = map(int, input().split())
     start = L + 1
     end = P // C if P / C == P // C else P // C + 1
     ans = 0
+
     while start < end:
-        bitween = (end - start) // 2
-        start += bitween
-        end -= bitween
+        mid = (end - start) // 2
+        start = mid - mid // 2
+        end = mid + mid // 2
         ans += 1
     print(ans)
