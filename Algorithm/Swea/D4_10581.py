fa254c87d97adb513fce9ce6002b964e2b9f5e49
hongyong3 <chy66822495@gmail.com>
2020-08-20 16:40:32 +0900

    D568 | 20200820 PM | Algorithm

--- Algorithm/Swea/D4_10581.py
+++ Algorithm/Swea/D4_10581.py
@@ -2,11 +2,28 @@ import sys
 sys.stdin = open("D4_10581_input.txt", "r")
 
 # def binary(a, b):
+#     cnt = 0
 #     while a < b:
 #         mid = (b - a) // 2
-#         a += mid // 2
-#         b -= mid // 2
+#         if not mid:
+#             mid = 1
+#         a += mid
+#         b -= mid
+#         cnt += 1
+#     return cnt
 #
+# T = int(input())
+# for test_case in range(T):
+#     L, P, C = map(int, input().split())
+#     start = L
+#     end = P // C if P / C == P // C else P // C + 1
+#     ans = binary(start, end)
+#     # while start < end:
+#     #     between = (end - start) // 2
+#     #     start += between
+#     #     end -= between
+#     #     ans += 1
+#     print(ans)
 
 # T = int(input())
 # for test_case in range(T):
@@ -14,23 +31,31 @@ sys.stdin = open("D4_10581_input.txt", "r")
 #     start = L + 1
 #     end = P // C if P / C == P // C else P // C + 1
 #     ans = 0
+#
 #     while start < end:
-#         bitween = (end - start) // 2
-#         start += bitween
-#         end -= bitween
+#         mid = (end - start) // 2
+#         start = mid - mid // 2
+#         end = mid + mid // 2
 #         ans += 1
 #     print(ans)
+
+def binary(a, b):
+    cnt = 0
+    while a < b:
+        mid = (b - a) // 2
+        if not mid:
+            mid = 1
+        a += mid
+        b -= mid
+        cnt += 1
+    return cnt
 
 T = int(input())
 for test_case in range(T):
     L, P, C = map(int, input().split())
-    start = L + 1
-    end = P // C if P / C == P // C else P // C + 1
-    ans = 0
-
-    while start < end:
-        mid = (end - start) // 2
-        start = mid - mid // 2
-        end = mid + mid // 2
-        ans += 1
-    print(ans)
+    start1, start2 = L, L + 1
+    end1, end2 = P // C, P // C + 1
+    # end = P // C if P / C == P // C else P // C + 1
+    ans = binary(start1, end1)
+    ans += binary(start2, end2)
+    print("#{} {}".format(test_case + 1, ans))
