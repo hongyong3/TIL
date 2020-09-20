5ada142434271618775e9e323fd5cfc480842d40
hongyong3 <chy66822495@gmail.com>
2020-09-20 22:18:34 +0900

    D598 | 20200920 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -1073,3 +1073,33 @@ if n = 8:
     y = (4, 7, 12, 20, 33, 54) 순
 54
 '''
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    mat = 0
+
+    for b in range(2, N + 1):
+        num = N % b
+        x = N
+        chk = 1
+        while x:
+            if x % b != num:
+                chk = 0
+                break
+            x //= b
+        if chk:
+            mat = b
+            break
+
+    if not mat:
+        for i in range(1, N + 1):
+            if i * i <= N:
+                if N % i:
+                    continue
+                x = i
+                base = N // x - 1
+                if base > x:
+                    mat = base
+        mat = N + 1
+    print("#{} {}".format(test_case + 1, mat))
