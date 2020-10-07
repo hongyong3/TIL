c8ba88d010789107d092bae6b913fb146e824c59
hongyong3 <chy66822495@gmail.com>
2020-10-07 19:26:22 +0900

    D614 | 20201007 PM | Algorithm

---
+++ Algorithm/Swea/D5_4583.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open("D5_4583_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    M, K = map(int, input().split())
+    num = ['0', '1', '2', '3', '4', '5', '6']
+    arr = ['0123456']
+    data = [list(map(int, input().split())) for _ in range(M)]
+    c = True
+    while c:
+        for a, b in data:
+            num[a - 1], num[b - 1] = num[b - 1], num[a - 1]
+            joinNum = ''.join(num)
+            if joinNum not in arr:
+                arr.append(joinNum)
+            else:
+                cycle = len(arr)
+                c = False
+                break
+    ans = arr[M] if M > K else arr[K % cycle]
+    print("#{} {}".format(test_case + 1, ans))
