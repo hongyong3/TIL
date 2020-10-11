f3f7afec6a27208aeec025039ba2cf3c6da85cca
hongyong3 <chy66822495@gmail.com>
2020-10-11 22:15:36 +0900

    D617 | 20201011 PM | Algorithm

--- Algorithm/Swea/D5_10727.py
+++ Algorithm/Swea/D5_10727.py
@@ -1,5 +1,9 @@
 import sys
 sys.stdin = open("D5_10727_input.txt", "r")
+'''
+업데이트 하고 해당셀이, 해당행, 해당열에서 가장 큰값이면 ans += 1
+시간초과 조심하자 
+'''
 
 T = int(input())
 for test_case in range(T):
@@ -7,7 +11,7 @@ for test_case in range(T):
     ans = 0
     data = [list(map(int, input().split())) for _ in range(n)]
     maxArr = [[0] * m for _ in range(n)]
-    
+
     for i in range(n):
         gm = max(data[i])
         k = 0
