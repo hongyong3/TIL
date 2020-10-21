81fd9dc2a2adafce3e0227b3b19b1f2a9ce0e0dd
hongyong3 <chy66822495@gmail.com>
2020-10-21 18:10:47 +0900

    D627 | 20201021 PM | Algorithm

--- Algorithm/Swea/D5_4740.py
+++ Algorithm/Swea/D5_4740.py
@@ -1,30 +1,64 @@
 import sys
 sys.stdin = open("D5_4740_input.txt", "r")
 
-def down():
+dx = [- 1, 1, 0, 0] # 상 하 좌 우
+dy = [0, 0, - 1, 1]
+
+def delete():
     pass
 
-def up():
-    pass
+def up(arr):
+    chk = False
+    if len(set(data[0])) > 1:
+        return
+    for i in range(N - 1):
+        for j in range(M):
+            data[i][j] = data[i + 1][j]
+    for i in range(M):
+        data[- 1][i] = arr[i]
+        if arr[i] == '#':
+            chk = True
+
+    if chk:
+        for i in range(M):
+            
 
 def left():
-    pass
+    for i in range(N):
+        for j in range(M):
+            if data[i][j] == '#':
+                for k in range(j + 1, M):
+                    if data[i][k] != '#':
+                        data[i][j], data[i][k] = data[i][k], data[i][j]
+                        break
+
 
 def right():
-    pass
+    for i in range(N):
+        for j in range(M - 1, - 1, - 1):
+            if data[i][j] == '#':
+                for k in range(j - 1, - 1, - 1):
+                    if data[i][k] != '#':
+                        data[i][j], data[i][k] = data[i][k], data[i][j]
+                        break
+
 
 T = int(input())
-for test_case in range(T):
+for test_case in range(1):
     N, M, Q = map(int, input().split())
-    data = [input() for _ in range(N)]
+    data = [list(map(str, input())) for _ in range(N)]
 
     for _ in range(Q):
-        q = input()
+        q = list(map(str, input()))
         if q == 'D':
             pass
         elif q == 'L':
-            pass
+            left()
         elif q == 'R':
-            pass
+            right()
         else:
-            pass
+            up(q[2:])
+
+    for i in data:
+        print(''.join(i))
+    print()
