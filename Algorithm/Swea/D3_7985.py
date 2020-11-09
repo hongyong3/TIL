f0223651888d16602cf3b96e1fb3a6d6ea401925
hongyong3 <chy66822495@gmail.com>
2020-11-09 18:42:06 +0900

    D645 | 20201109 PM | Algorithm

--- Algorithm/Swea/D3_7985.py
+++ Algorithm/Swea/D3_7985.py
@@ -1,7 +1,18 @@
 import sys
 sys.stdin = open("D3_7985_input.txt", "r")
 
+def solve(s, e, d):
+    global N
+    if  d > N:
+        return
+    mid = (e - s) // 2
+
+
 T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
+    tree = [pow(2, i) for i in range(11)]
+    for i in range(tree[N] - 1):
+        tree[i] = data[i]
+    print(tree)
