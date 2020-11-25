611da32a1a795dab7555d5edb79895fa693a19c5
hongyong3 <chy66822495@gmail.com>
2020-11-25 17:12:27 +0900

    D659 | 20201125 PM | Algorithm

--- Algorithm/Swea/D3_7227.py
+++ Algorithm/Swea/D3_7227.py
@@ -3,16 +3,33 @@ sys.stdin = open("D3_7227_input.txt", "r")
 
 '''
 나올 수 있는 경우의 수
-nC(n//2) % 2?
+nC(n//2) // 2
 '''
-def solve(n, cnt):
-    if cnt == N // 2:
-        pass
+case = [0, 1, 3, 10, 35, 126, 462, 1716, 6435, 24310, 92378]
 
-    for i in range(N):
+def solve(n, cnt):
+    global ans, c
+
+    if not c or not ans:
+        return
+    if cnt == N // 2:
+        c -= 1
+        x, y = 0, 0
+        for i in range(N):
+            if chk[i]:
+                x += data[i][0]
+                y += data[i][1]
+            else:
+                x -= data[i][0]
+                y -= data[i][1]
+        if ans > x * x + y * y:
+            ans = x * x + y * y
+        return
+
+    for i in range(n, N):
         if not chk[i]:
             chk[i] = 1
-            solve(n + 1, cnt + 1)
+            solve(i + 1, cnt + 1)
             chk[i] = 0
 
 T = int(input())
@@ -21,3 +38,6 @@ for test_case in range(T):
     data = [list(map(int, input().split())) for _ in range(N)]
     ans = float('inf')
     chk = [0] * N
+    c = case[N // 2]
+    solve(0, 0)
+    print("#{} {}".format(test_case + 1, ans))
--- Algorithm/Swea/D4_1494.py
+++ Algorithm/Swea/D4_1494.py
@@ -31,31 +31,68 @@ from itertools import combinations
 #     solve(0, - 1)
 #     print("#{} {}".format(test_case + 1, answer))
 
-def solve(n, count):
-    global answer
-    if count == N // 2:
+# def solve(n, count):
+#     global answer
+#     if count == N // 2:
+#         x, y = 0, 0
+#         for i in range(N):
+#             if check[i] == 1:
+#                 x += data[i][0]
+#                 y += data[i][1]
+#             else:
+#                 x -= data[i][0]
+#                 y -= data[i][1]
+#         if answer > x * x + y * y:
+#             answer = x * x + y * y
+#         return
+#     for i in range(n, N):
+#         if check[i] == 0:
+#             check[i] = 1
+#             solve(i + 1, count + 1)
+#             check[i] = 0
+#
+# T = int(input())
+# for test_case in range(2):
+#     N = int(input())
+#     data = [list(map(int, input().split())) for _ in range(N)]
+#     answer = float('inf')
+#     check = [0] * 20
+#     solve(0, 0)
+#     print("#{} {}".format(test_case + 1, answer))
+
+# 가지치기 추가
+case = [0, 1, 3, 10, 35, 126, 462, 1716, 6435, 24310, 92378]
+
+def solve(n, cnt):
+    global ans, c
+    if not c or not ans:
+        return
+    if cnt == N // 2:
+        c -= 1
         x, y = 0, 0
         for i in range(N):
-            if check[i] == 1:
+            if chk[i]:
                 x += data[i][0]
                 y += data[i][1]
             else:
                 x -= data[i][0]
                 y -= data[i][1]
-        if answer > x * x + y * y:
-            answer = x * x + y * y
+        if ans > x * x + y * y:
+            ans = x * x + y * y
         return
+
     for i in range(n, N):
-        if check[i] == 0:
-            check[i] = 1
-            solve(i + 1, count + 1)
-            check[i] = 0
+        if not chk[i]:
+            chk[i] = 1
+            solve(i + 1, cnt + 1)
+            chk[i] = 0
 
 T = int(input())
-for test_case in range(2):
+for test_case in range(T):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(N)]
-    answer = float('inf')
-    check = [0] * 20
+    ans = float('inf')
+    chk = [0] * N
+    c = case[N // 2]
     solve(0, 0)
-    print("#{} {}".format(test_case + 1, answer))
+    print("#{} {}".format(test_case + 1, ans))
