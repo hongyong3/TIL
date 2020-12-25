ba8786fac24eaeaaf2e62e2f8f92407487f6453a
hongyong3 <chy66822495@gmail.com>
2020-12-25 21:31:41 +0900

    D687 | 20201225 PM | Algorithm_Christmas

--- Algorithm/Swea/D3_11285.py
+++ Algorithm/Swea/D3_11285.py
@@ -1,9 +1,12 @@
 import sys
 sys.stdin = open("D3_11285_input.txt", "r")
 
+# RunTime Error...
 T = int(input())
 for test_case in range(T):
     N = int(input())
+    ans = 0
     for _ in range(N):
         x, y = map(int, input().split())
-        distance = (x ** 2 + y ** 2) ** 0.5
+        ans += int(220 - pow(x ** 2 + y ** 2, 0.5)) // 20
+    print("#{} {}".format(test_case + 1, ans))
