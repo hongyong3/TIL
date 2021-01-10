ad6058da4a86ed73e367556fcffcbd78a1f8c8aa
hongyong3 <chy66822495@gmail.com>
2021-01-10 20:22:36 +0900

    D702 | 20211010 PM | Algorithm

--- Algorithm/Swea/D4_10908.py
+++ Algorithm/Swea/D4_10908.py
@@ -4,15 +4,19 @@ sys.stdin = open("D4_10908_input.txt", "r")
 def pow(a, n):
     if n == 0:
         return 1
-    return a * pow(a, n - 1)
+    else:
+        return a * pow(a, n - 1)
 
 T = int(input())
+ans = []
 for test_case in range(T):
     N = int(input())
-    ans, cnt = N + 1, 0
+    M, cnt = N, 0
 
     while N >= 1:
         if N % 2:
             cnt += 1
         N //= 2
-    print("#{} {}".format(test_case + 1, ans - pow(2, cnt)))
+    ans.append(M + 1 - int(pow(2, cnt)))
+for i in range(T):
+    print("#{} {}".format(i + 1, ans[i]))
