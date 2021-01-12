ea71453a32799476b2967e449c2a748653b10b2e
hongyong3 <chy66822495@gmail.com>
2021-01-12 17:53:01 +0900

    D704 | 20210112 PM | Algorithm

--- Algorithm/Swea/D5_3363.py
+++ Algorithm/Swea/D5_3363.py
@@ -1,11 +1,45 @@
 import sys
 sys.stdin = open("D5_3363_input.txt", "r")
 
+# Runtime Error...
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     arr, cnt = [], 0
+#     for _ in range(N):
+#         x, y, z = map(int, input().split())
+#         arr.append((x, y, z))
+#         if cnt < y:
+#             cnt = y
+#     num = [0] * (cnt + 1)
+#
+#     for a, b, c in arr:
+#         for i in range(a, b + 1, c):
+#             num[i] += 1
+#
+#     for i in range(1, cnt + 1):
+#         if num[i] % 2:
+#             print("#{} {} {}".format(test_case + 1, i, num[i]))
+#             break
+#     else:
+#         print("#{} no corruption".format(test_case + 1))
+
+
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    arr = [0] * N
-    for i in range(N):
+    arr, ans = [], 0
+    for _ in range(N):
         x, y, z = map(int, input().split())
-        temp = (x + y) * (y - x + 1) // 2
-        arr[i] = temp
+        arr.append((x, y, z))
+        for i in range(x, y + 1, z):
+            ans ^= i
+    if not ans:
+        print("#{} no corruption".format(test_case + 1))
+    else:
+        cnt = 0
+        for j in arr:
+            if j[0] <= ans <= j[1]:
+                if not (ans - j[0]) % j[2]:
+                    cnt += 1
+        print("#{} {} {}".format(test_case + 1, ans, cnt))
