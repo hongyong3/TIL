a16ba9acbe45a67e3d85ed746f798ed749eb3efe
hongyong3 <chy66822495@gmail.com>
2021-01-15 18:13:22 +0900

    D707 | 20210115 PM | Algorithm

--- Algorithm/Swea/D3_11315.py
+++ Algorithm/Swea/D3_11315.py
@@ -8,15 +8,14 @@ def dfs(x, y, cnt):
     global ans
     if cnt == 5:
         ans = "YES"
-        return
+
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if not (0 <= nx < N and 0 <= ny < N):
-            continue
-        if data[nx][ny] == 'o':
-            dfs(nx, ny, cnt + 1)
+        if 0 <= nx < N and 0 <= ny < N and data[nx][ny] == 'o':
+            # 한 방향으로만 갈 수 있도록 ex) while이라던가 방향을 고정하기...
+
 
 
 T = int(input())
@@ -24,9 +23,10 @@ for test_case in range(T):
     N = int(input())
     data = [input() for _ in range(N)]
     ans = "NO"
+    print(data)
 
     for i in range(N):
         for j in range(N):
             if data[i][j] == 'o':
-                dfs(i, j, 1)
+                dfs(i, j, 0)
     print(ans)
