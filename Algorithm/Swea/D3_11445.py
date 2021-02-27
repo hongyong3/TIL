5eb6a6be09d5ab6455c74968e8d4a290d69d780f
hongyong3 <chy66822495@gmail.com>
2021-02-27 17:08:38 +0900

    D745 | 20210227 PM | Algorithm

--- Algorithm/Swea/D3_11445.py
+++ Algorithm/Swea/D3_11445.py
@@ -1,66 +1,10 @@
 import sys
 sys.stdin = open("D3_11445_input.txt", "r")
 
-# 76 / 100
-# T = int(input())
-# for test_case in range(3):
-#     P = input()
-#     Q = input()
-#     ans = "Y"
-#     chk = True
-#     i, j = 0, 0
-#
-#     while chk and len(P) > i and len(Q) > j:
-#         if P[i] > Q[j]:
-#             chk = False
-#             ans = 'N'
-#         elif P[i] == Q[j]:
-#             i += 1
-#             j += 1
-#         else:
-#             break
-#     if i == len(P):
-#         if j < len(Q) and Q[j] == 'a':
-#             ans = 'N'
-#     print("#{} {}".format(test_case + 1, ans))
-
-# 76 / 100
-# T = int(input())
-# for test_case in range(T):
-#     p, q = input().replace(" ", ""), input().replace(" ", "")
-#     ans = 'Y'
-#     chk = True
-#     i, j = 0, 0
-#
-#     while chk and i < len(p):
-#         if p[i] < q[j]:
-#             chk = False
-#         else:
-#             i += 1
-#             j += 1
-#     if len(q) - len(p) == 1 and q[- 1] == 'a':
-#         ans = 'N'
-#     print("#{} {}".format(test_case + 1, ans))
-
-# 76 / 100
-# ..? 뭐지..
-# T = int(input())
-# for test_case in range(T):
-#     p, q = input().replace(" ", ""), input().replace(" ", "")
-#     if len(q) - len(p) == 1:
-#         if q[- 1] == 'a':
-#             ans = 'N'
-#     else:
-#         ans = 'Y'
-#     print("#{} {}".format(test_case + 1, ans))
-
-# 93 / 100
 T = int(input())
 for test_case in range(T):
-    p, q = input().strip().replace(" ", ""), input().strip().replace(" ", "")
-    if len(q) - len(p) == 1:
-        if q[- 1] == 'a':
-            ans = 'N'
-    else:
-        ans = 'Y'
+    p, q = input().strip(), input().strip()
+    ans = 'Y'
+    if len(q) - len(p) == 1 and p == q[: - 1] and q[- 1] == 'a':
+        ans = 'N'
     print("#{} {}".format(test_case + 1, ans))
--- Algorithm/Swea/D4_11545.py
+++ Algorithm/Swea/D4_11545.py
@@ -1,46 +1,49 @@
 import sys
 sys.stdin = open("D4_11545_input.txt", "r")
 
-dx = [0, 1, 1, - 1, 0, - 1, 1, - 1] # 우 하 우하 상 좌 우상 좌하 좌상
-dy = [1, 0, 1, 0, - 1, 1, - 1, - 1]
-
 T = int(input())
 for test_case in range(T):
     data = [input() for _ in range(4)]
-    ans = "aa"
+    total = 0
     if test_case != T - 1:
         temp = input()
 
     for x in range(4):
         for y in range(4):
-            if data[x][y] == 'X':
-                for i in range(8):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    cnt = 1
-                    while 0 <= nx < 4 and 0 <= ny < 4:
-                        if data[nx][ny] == 'X' or data[nx][ny] == 'T':
-                            cnt += 1
-                        else:
-                            break
-                        nx += dx[i]
-                        ny += dy[i]
-                        if cnt == 4:
-                            ans = "X won"
-                            break
-            elif data[x][y] == 'Y':
-                for i in range(8):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    cnt = 1
-                    while 0 <= nx < 4 and 0 <= ny < 4:
-                        if data[nx][ny] == 'Y' or data[nx][ny] == 'T':
-                            cnt += 1
-                        else:
-                            break
-                        nx += dx[i]
-                        ny += dy[i]
-                        if cnt == 4:
-                            ans = "Y won"
-                            break
-    print(ans)
+            if data[x][y] != '.':
+                total += 1
+
+    k = 0
+    while k < 4:
+        OGcnt, OScnt, XGcnt, XScnt, OCcnt1, OCcnt2, XCcnt1, XCcnt2 = 0, 0, 0, 0, 0, 0, 0, 0
+        for i in range(4):
+            if data[k][i] in 'OT':
+                OGcnt += 1
+            if data[k][i] in 'XT':
+                XGcnt += 1
+            if data[i][k] in 'OT':
+                OScnt += 1
+            if data[i][k] in 'XT':
+                XScnt += 1
+            if data[i][i] in 'OT':
+                OCcnt1 += 1
+            if data[i][i] in 'XT':
+                XCcnt1 += 1
+            if data[3 - i][i] in 'OT':
+                OCcnt2 += 1
+            if data[3 - i][i] in 'XT':
+                XCcnt2 += 1
+
+        if OGcnt == 4 or OScnt == 4 or OCcnt1 == 4 or OCcnt2 == 4:
+            print("#{} O won".format(test_case + 1))
+            break
+        if XGcnt == 4 or XScnt == 4 or XCcnt1 == 4 or XCcnt2 == 4:
+            print("#{} X won".format(test_case + 1))
+            break
+        k += 1
+
+    if k == 4:
+        if total == 16:
+            print("#{} Draw".format(test_case + 1))
+        else:
+            print("#{} Game has not completed".format(test_case + 1))
