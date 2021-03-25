665f3ffe7e11d4c766e89008dcb856261b9b4de8
hongyong3 <chy66822495@gmail.com>
2021-03-25 23:18:48 +0900

    D768 | 20210325 PM | Algorithm

--- Algorithm/Swea/D5_11689.py
+++ Algorithm/Swea/D5_11689.py
@@ -1,25 +1,29 @@
 import sys
 sys.stdin = open("D5_11689_input.txt", "r")
 
+# Runtime Error..?
+# arr = []
 T = int(input())
-arr = []
 for test_case in range(T):
     a, b = map(int, input().split())
     ans = 0
-    # if a == b:
+
     if a == 1 or b == 1:
         ans = max(a, b) - 1
+
     else:
         while a != b:
             if a == 1 or b == 1:
                 ans += max(a, b) - 1
                 break
+
             if a > b:
                 a -= b
             else:
                 b -= a
             ans += 1
-    # print("#{} {}".format(test_case + 1, ans))
-    arr.append("#{} {}".format(test_case + 1, ans))
-for i in arr:
-    print(i)
+
+    print("#{} {}".format(test_case + 1, ans))
+    # arr.append("#{} {}".format(test_case + 1, ans))
+# for i in arr:
+#     print(i)
