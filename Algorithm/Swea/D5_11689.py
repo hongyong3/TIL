43027ce57b1bd0a66a5469dc73cb729a2053a1e8
hongyong3 <chy66822495@gmail.com>
2021-03-27 23:28:16 +0900

    D770 | 20210327 PM | Algorithm

--- Algorithm/Swea/D5_11689.py
+++ Algorithm/Swea/D5_11689.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("D5_11689_input.txt", "r")
 
 # Runtime Error..?
-arr = []
+# arr = []
 T = int(input())
 for test_case in range(T):
     a, b = map(int, input().split())
@@ -24,6 +24,6 @@ for test_case in range(T):
             ans += 1
 
     # print("#{} {}".format(test_case + 1, ans))
-    arr.append("#{} {}".format(test_case + 1, ans))
-for i in arr:
-    print(i)
+#     arr.append("#{} {}".format(test_case + 1, ans))
+# for i in arr:
+#     print(i)
