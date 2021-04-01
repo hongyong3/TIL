baa2106b1a19256ca6c23ba316d4fecc94f3b2a3
hongyong3 <chy66822495@gmail.com>
2021-04-01 23:25:18 +0900

    D775 | 20210401 PM | Algorithm

--- Algorithm/Swea/D5_11593.py
+++ Algorithm/Swea/D5_11593.py
@@ -1,6 +1,30 @@
 import sys
 sys.stdin = open("D5_11593_input.txt", "r")
 
+'''
+from collections import Counter as Multiset
+
+
+def multiset_combinations(ms, n):
+    assert sum(ms.values()) >= n
+    def f(ms_res, curr_val, ms_rem):
+        nonlocal n
+        if sum(ms_res.values()) == n:  #ideally len would return the number of total elements
+            yield ms_res
+        else:
+            for val in set(ms_rem):
+                if val >= curr_val:
+                    # for each unique value in remaining multiset equal to or larger than the current value
+                    val_ms = Multiset((val,))  # ideally I wouldn't need to wrap a singleton multiset
+                    # add that value to the result multiset and remove it from remaining multiset
+                    yield from f(ms_res + val_ms, val, ms_rem - val_ms)
+                    # recurse
+    yield from f(Multiset(), sorted(ms.keys())[0], ms)
+
+for m in multiset_combinations(Multiset((3, 3, 4, 4, 4, 5)), 3):
+    print(list(m.elements()))
+'''
+
 
 # multiset permutation?
 fact = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000]
