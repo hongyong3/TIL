22158eaec121fc299f5ba3555075544bbf9b0627
hongyong3 <chy66822495@gmail.com>
2021-04-07 22:15:16 +0900

    D781 | 20210407 PM | Algorithm

--- Algorithm/Swea/D3_11346.py
+++ Algorithm/Swea/D3_11346.py
@@ -1,16 +1,16 @@
 import sys
 sys.stdin = open("D3_11346_input.txt", "r")
 
-# Runtime Error
+
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    data = set(map(int, input().split()))
+    data = list(map(int, input().split()))
     a, b = 0, 0
     for i in data:
         if i % 2:
             a += 1
         else:
             b += 1
-    print("#{} {}".format(test_case + 1, min(a, b) * 2 + 1))
+    print("#{} {}".format(test_case + 1, a + b if a == b else min(a, b) * 2 + 1))
     
