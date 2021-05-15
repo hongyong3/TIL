7ec5ff3a7c5982940931e5b92fc5b6b96b36ff79
hongyong3 <chy66822495@gmail.com>
2021-05-15 19:40:22 +0900

    D817 | 20210515 PM | Algorithm

--- Algorithm/Swea/D3_12004.py
+++ Algorithm/Swea/D3_12004.py
@@ -4,4 +4,11 @@ sys.stdin = open("D3_12004_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    
+    ans = "Yes"
+    for i in range(1, 10):
+        b = N / i
+        if N // i == N / i and N / i < 10:
+            break
+    else:
+        ans = "No"
+    print("#{} {}".format(test_case + 1, ans))
