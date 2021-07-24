f39e8fc806b544ebd35800502e27d9085aa94d9c
hongyong3 <chy66822495@gmail.com>
2021-07-24 17:21:48 +0900

    D880 | 20210724 PM | Algorithm

--- Algorithm/Swea/D4_12222.py
+++ Algorithm/Swea/D4_12222.py
@@ -2,18 +2,16 @@ import sys
 sys.stdin = open("D4_12222_input.txt", "r")
 
 T = int(input())
-for test_case in range(T):
+for test_case in range(1):
     S = input()
-    s, n = 0, len(S)    # 시작, 문자열길이
-    ans, i = 1, 1
-    a = S[s]
+    lS = len(S)
+    ans = 0
+    a, idx = S[0], 1
 
-    while s < n:
-        b = S[s + i]
+    while idx < lS:
+        b = S[idx]
         if a == b:
-            i += 1
-        else:
-            s += i
-            i = 1
-            a = b
-            ans += 1
+            if idx + 1 == lS:
+                break
+            if a == S[idx + 1]:
+                a 
