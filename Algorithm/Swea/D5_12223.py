91b17e362dcf1c0d32ab30a5ed2e3b48a823a9e0
hongyong3 <chy66822495@gmail.com>
2021-07-30 21:48:39 +0900

    D884 | 20210730 PM | Algorithm

---
+++ Algorithm/Swea/D5_12223.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("D5_12223_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    data = [list(map(int, input().split())) for _ in range(N)]
+    print(data)
