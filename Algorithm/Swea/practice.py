11d8f452673c43c12fddf394e20ebb843a6a9d8e
hongyong3 <chy66822495@gmail.com>
2021-08-02 21:46:54 +0900

    D887 | 20210802 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -370,3 +370,21 @@ for _ in range(m):
     for idx in range(i - 1, j):
         arr[idx] = k
 print(*arr)
+
+# boj 4375
+while True:
+    try:
+        x = int(input())
+    except EOFError:
+        break
+    if x == 1:
+        print('1')
+        continue
+    num = 1
+    cnt = 1
+    while True:
+        num = num * 10 + 1
+        cnt += 1
+        if (num % x) == 0:
+            print(cnt)
+            break
