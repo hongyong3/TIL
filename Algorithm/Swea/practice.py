baa2b9adb6dcdce5f035bf413dd5c106631b82df
hongyong3 <chy66822495@gmail.com>
2021-08-10 21:59:13 +0900

    D894 | 20210810 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -564,17 +564,66 @@ input = sys.stdin.readline
 #         print(ans)
 
 # boj 18141
-def chk():
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                x = (arr[i] - arr[j]) / arr[k]
-                y = (arr[i] - arr[k]) / arr[j]
-                z = (arr[j] - arr[k]) / arr[i]
-                if not(x == int(x) and y == int(y) and z == int(z)):
-                    return False
-    return True
+# def chk():
+#     for i in range(n - 2):
+#         for j in range(i + 1, n - 1):
+#             for k in range(j + 1, n):
+#                 x = (arr[i] - arr[j]) / arr[k]
+#                 y = (arr[i] - arr[k]) / arr[j]
+#                 z = (arr[j] - arr[k]) / arr[i]
+#                 if not(x == int(x) and y == int(y) and z == int(z)):
+#                     return False
+#     return True
+#
+# n = int(input())
+# arr = list(map(int, input().split()))
+# print("yes" if chk() else "no")
 
-n = int(input())
-arr = list(map(int, input().split()))
-print("yes" if chk() else "no")
+# swea no.1
+# T = int(input())
+# for test_case in range(T):
+#     A, B = map(int, input().split())
+#     ans = (A + B) % 24
+#     print("#{} {}".format(test_case + 1, ans))
+
+# swea no.2
+# 19 / 20...
+T = int(input())
+for test_case in range(T):
+    S, N = input().split()
+    N, l = int(N), len(S)
+    total, temp = 0, 1
+    ans, arr = '', []
+
+    while True:
+        temp *= l
+        if N >= total + temp:
+            arr.append(temp)
+            total += temp
+        else:
+            diff = N - total
+            break
+
+    idx = len(arr)
+    if diff:
+        idx += 1
+    if idx == 1 and diff == 0:
+        ans = S
+    else:
+        for i in arr[:: -1]:
+            if diff == i:
+                ans += S[0]
+                diff -= i
+            else:
+                mok, nam = diff // i, diff % i
+                diff -= mok * i
+                if mok == l or nam == 0:
+                    ans += S[- 1]
+                else:
+                    ans += S[mok]
+        if diff == l:
+            ans += S[- 1]
+        else:
+            if len(ans) != idx:
+                ans += S[diff - 1]
+    print("#{} {}".format(test_case + 1, ans))
