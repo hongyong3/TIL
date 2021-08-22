0c3f68638b46b80a380ac339417787ded95250fd
hongyong3 <chy66822495@gmail.com>
2021-08-22 20:47:07 +0900

    D905 | 20210822 PM | Algorithm

---
+++ Algorithm/Swea/D4_10086.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("D4_10086_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N, K = map(int, input().split())
+    arr = list(map(int, input().split()))
+    ans = 0
+    idx = 0
+    while True:
+        if arr[idx + K]:
+            idx = K
+            
