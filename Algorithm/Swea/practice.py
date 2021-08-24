f137eead1e1b6f6ff7c5e7bd87d3951293db9d1b
hongyong3 <chy66822495@gmail.com>
2021-08-24 21:42:58 +0900

    D907 | 20210824 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -588,42 +588,50 @@ input = sys.stdin.readline
 
 # swea no.2
 # 19 / 20...
+# T = int(input())
+# for test_case in range(T):
+#     S, N = input().split()
+#     N, l = int(N), len(S)
+#     total, temp = 0, 1
+#     ans, arr = '', []
+#
+#     while True:
+#         temp *= l
+#         if N >= total + temp:
+#             arr.append(temp)
+#             total += temp
+#         else:
+#             diff = N - total
+#             break
+#
+#     idx = len(arr)
+#     if diff:
+#         idx += 1
+#     if idx == 1 and diff == 0:
+#         ans = S
+#     else:
+#         for i in arr[:: -1]:
+#             if diff == i:
+#                 ans += S[0]
+#                 diff -= i
+#             else:
+#                 mok, nam = diff // i, diff % i
+#                 diff -= mok * i
+#                 if mok == l or nam == 0:
+#                     ans += S[- 1]
+#                 else:
+#                     ans += S[mok]
+#         if diff == l:
+#             ans += S[- 1]
+#         else:
+#             if len(ans) != idx:
+#                 ans += S[diff - 1]
+#     print("#{} {}".format(test_case + 1, ans))
+
+# SWEA No.1
 T = int(input())
 for test_case in range(T):
-    S, N = input().split()
-    N, l = int(N), len(S)
-    total, temp = 0, 1
-    ans, arr = '', []
-
-    while True:
-        temp *= l
-        if N >= total + temp:
-            arr.append(temp)
-            total += temp
-        else:
-            diff = N - total
-            break
-
-    idx = len(arr)
-    if diff:
-        idx += 1
-    if idx == 1 and diff == 0:
-        ans = S
-    else:
-        for i in arr[:: -1]:
-            if diff == i:
-                ans += S[0]
-                diff -= i
-            else:
-                mok, nam = diff // i, diff % i
-                diff -= mok * i
-                if mok == l or nam == 0:
-                    ans += S[- 1]
-                else:
-                    ans += S[mok]
-        if diff == l:
-            ans += S[- 1]
-        else:
-            if len(ans) != idx:
-                ans += S[diff - 1]
-    print("#{} {}".format(test_case + 1, ans))
+    N = int(input())
+    A0, B0, C0 = map(int, input().split())
+    A1, B1, C1 = map(int, input().split())
+    ans = 0
