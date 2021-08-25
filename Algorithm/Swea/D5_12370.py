64606986f074dcd0595b0c28045a67d5462efd45
hongyong3 <chy66822495@gmail.com>
2021-08-25 21:19:08 +0900

    D908 | 20210825 PM | Algorithm

---
+++ Algorithm/Swea/D5_12370.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("D5_12370_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    A0, B0, C0 = map(int, input().split())
+    A1, B1, C1 = map(int, input().split())
+    ans = 0
