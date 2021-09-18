fa97f0ba6434dd449ce5fdd4923be4adcdea4396
hongyong3 <chy66822495@gmail.com>
2021-09-18 18:46:19 +0900

    D929 | 20210918 PM | Algorithm

--- Algorithm/Swea/D3_12741.py
+++ Algorithm/Swea/D3_12741.py
@@ -35,21 +35,21 @@ sys.stdin = open("D3_12741_input.txt", "r")
 #     print(i)
 
 # Runtime Error..
-T = int(input())
-ans = []
-for test_case in range(T):
-    A, B, C, D = map(int, input().split())
-    if B <= C:
-        ans.append("#{} {}".format(test_case + 1, 0))
-    elif A <= C and C < B and B <= D:
-        ans.append("#{} {}".format(test_case + 1, B - C))
-    elif A <= C and D <= B:
-        ans.append("#{} {}".format(test_case + 1, D - C))
-    elif D <= A:
-        ans.append("#{} {}".format(test_case + 1, 0))
-    elif C <= A and A < D and D <= B:
-        ans.append("#{} {}".format(test_case + 1, A - D))
-    elif C <= A and B <= D:
-        ans.append("#{} {}".format(test_case + 1, B - A))
-for i in ans:
-    print(i)
+# T = int(input())
+# ans = []
+# for test_case in range(T):
+#     A, B, C, D = map(int, input().split())
+#     if B <= C:
+#         ans.append("#{} {}".format(test_case + 1, 0))
+#     elif A <= C and C < B and B <= D:
+#         ans.append("#{} {}".format(test_case + 1, B - C))
+#     elif A <= C and D <= B:
+#         ans.append("#{} {}".format(test_case + 1, D - C))
+#     elif D <= A:
+#         ans.append("#{} {}".format(test_case + 1, 0))
+#     elif C <= A and A < D and D <= B:
+#         ans.append("#{} {}".format(test_case + 1, A - D))
+#     elif C <= A and B <= D:
+#         ans.append("#{} {}".format(test_case + 1, B - A))
+# for i in ans:
+#     print(i)
