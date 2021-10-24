5321085a216b012898d4d77762fda33a8832f893
hongyong3 <chy66822495@gmail.com>
2021-10-24 22:52:23 +0900

    D958 | 20211024 PM | Algorithm

--- Algorithm/Swea/4317.py
+++ Algorithm/Swea/4317.py
@@ -1,17 +1,17 @@
 import sys
 sys.stdin = open("4317_input.txt", "r")
 
-# Runtime Error
 def chk(x, y):
-    if x + 1 >= H or y + 1 >= W:
-        return False
     if arr[x][y] or arr[x + 1][y] or arr[x][y + 1] or arr[x + 1][y + 1]:
+        return False
+    if x + 1 >= H or y + 1 >= W:
         return False
     return True
 
 
 def dfs(x, y, cnt):
     global ans
+
     if x >= H - 1:
         x = 0
         y += 1
@@ -36,14 +36,10 @@ def dfs(x, y, cnt):
     dfs(x + 1, y, cnt)
 
 T = int(input())
-dp = [[- 1] * 24 for _ in range(1024)]
 for test_case in range(T):
     H, W = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(H)]
-
-    for i in range(H):
-        for j in range(W):
-            dp[i][j] = - 1
+    dp = [[- 1] * 24 for _ in range(1 << H)]
     ans = 0
     dfs(0, 0, 0)
     print("#" + str(test_case + 1) + ' ' + str(ans))
