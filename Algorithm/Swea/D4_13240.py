408a530a489a79a46b34330cfbea0ee57e32988f
hongyong3 <chy66822495@gmail.com>
2021-12-11 20:05:17 +0900

    D991 | 20211211 PM | Algorithm

---
+++ Algorithm/Swea/D4_13240.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open("D4_13240_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    H, W, N = map(int, input().split())
+    arr = input().split()
+    wordLen = len(max(arr))
+    # garo, sero = float('inf'), float('inf')
+    # for i in arr:
+    #     minGaro = W // len(i)
+    #     if minGaro < garo:
+    #         garo = minGaro
+    # print(garo)
+    # for i in range(len(arr), - 1, - 1):
+    #
--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -1901,9 +1901,33 @@ input = sys.stdin.readline
 # print(*ans)
 
 # boj 10987
-s = input()
-ans = 0
-for i in s:
-    if i in 'aeiou':
-        ans += 1
-print(ans)
+# s = input()
+# ans = 0
+# for i in s:
+#     if i in 'aeiou':
+#         ans += 1
+# print(ans)
+
+# boj 21919
+import sys
+input = sys.stdin.readline
+def findPrime(n):
+    sieve = [True] * n
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:
+            for j in range(i + i, n, i):
+                sieve[j] = False
+    return [i for i in range(2, n) if sieve[i] == True]
+
+n = int(input())
+arr = set(map(int, input().split()))
+prime = findPrime(1000000)
+ans = 1
+for i in arr:
+    if i in prime:
+        ans *= i
+if ans == 1:
+    print(- 1)
+else:
+    print(ans)
