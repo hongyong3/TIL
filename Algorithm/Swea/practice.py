cd9892e00e06d54bb9bd3c4bd513e598950d2e5b
hongyong3 <chy66822495@gmail.com>
2022-01-10 23:54:26 +0900

    D1008 | 20220110 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -2295,16 +2295,16 @@ input = sys.stdin.readline
 #                 break
 
 # boj 10768
-m = int(input())
-d = int(input())
-if m > 2:
-    print("After")
-elif m < 2:
-    print("Before")
-else:
-    if d > 18:
-        print("After")
-    elif d < 18:
-        print("Before")
-    else:
-        print("Special")
+# m = int(input())
+# d = int(input())
+# if m > 2:
+#     print("After")
+# elif m < 2:
+#     print("Before")
+# else:
+#     if d > 18:
+#         print("After")
+#     elif d < 18:
+#         print("Before")
+#     else:
+#         print("Special")
