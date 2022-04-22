ddd24a6ecba37770393bf9228b403b9351127545
hongyong3 <chy66822495@gmail.com>
2022-04-22 22:05:56 +0900

    D1101 | 20220422 PM | Algorithm

--- Algorithm/Swea/D4_14179.py
+++ Algorithm/Swea/D4_14179.py
@@ -1,6 +1,31 @@
 import sys
 sys.stdin = open("D4_14179_input.txt", "r")
 
+# mod = 1000000007
+# T = int(input())
+# for test_case in range(T):
+#     N, K = map(int, input().split())
+#     arr = list(map(int, input().split()))
+#     arr2 = arr * 2
+#     inversion1, inversion2 = [0] * N, [0] * N
+#     ans = 0
+#
+#     for i in range(N):
+#         for j in range(i + 1, N):
+#             if arr[i] > arr[j]:
+#                 inversion1[i] += 1
+#
+#     for i in range(N):
+#         for j in range(i + 1, i + N):
+#             if arr2[i] > arr2[j]:
+#                 inversion2[i] += 1
+#
+#     for i in range(N):
+#         ans += (inversion1[i] * K + inversion2[i] * (K * (K - 1) // 2)) % mod
+#         ans %= mod
+#
+#     print("#{} {}".format(test_case + 1, ans % mod))
+
 mod = 1000000007
 T = int(input())
 for test_case in range(T):
@@ -15,7 +40,6 @@ for test_case in range(T):
             if arr[i] > arr[j]:
                 inversion1[i] += 1
 
-    for i in range(N):
         for j in range(i + 1, i + N):
             if arr2[i] > arr2[j]:
                 inversion2[i] += 1
