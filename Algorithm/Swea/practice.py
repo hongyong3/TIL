9ba62e2b0b74a4672ee64389306115e733f1c0d0
hongyong3 <chy66822495@gmail.com>
2022-05-03 22:55:29 +0900

    D1108 | 20220503 PM | Algorithm

--- Algorithm/Swea/practice.py
+++ Algorithm/Swea/practice.py
@@ -5662,13 +5662,13 @@ sys.stdin = open("practice_input.txt", "r")
 # print(*ans)
 
 # boj 9950
-while True:
-    l, w, a = map(int, input().split())
-    if l == w == a == 0:
-        break
-    if l == 0:
-        print(a // w, w, a)
-    elif w == 0:
-        print(l, a // l, a)
-    else:
-        print(l, w, l * w)
+# while True:
+#     l, w, a = map(int, input().split())
+#     if l == w == a == 0:
+#         break
+#     if l == 0:
+#         print(a // w, w, a)
+#     elif w == 0:
+#         print(l, a // l, a)
+#     else:
+#         print(l, w, l * w)
