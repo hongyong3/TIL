2c6aecb14a5598ef79232613e08caef21ee78374
hongyong3 <chy66822495@gmail.com>
2022-05-14 22:45:01 +0900

    D1115 | 20220514 PM | Algorithm

--- Algorithm/Swea/D4_14362.py
+++ Algorithm/Swea/D4_14362.py
@@ -1,7 +1,39 @@
 import sys
 sys.stdin = open("D4_14362_input.txt", "r")
 
+'''
+처음은 동쪽 [1, 0]
+dr = [[1, 0], [0, - 1], [- 1, 0], [0, 1]]
+dr = [[1, 0], [0, 1], [- 1, 0], [0, - 1]]
+L : 반시계방향
+    if  [1, 0]      ->  [0, - 1]    동 -> 북
+        [0, - 1]    ->  [- 1, 0]    북 -> 서
+        [- 1, 0]    ->  [0, 1]      서 -> 남
+        [0, 1]      ->  [1, 0]      남 -> 동
+R : 시계방향
+'''
 T = int(input())
 for test_case in range(T):
-    S = input()
+    command = input()
     ans = 'oo'
+    dd = 0
+    dr = [[1, 0], [0, 1], [- 1, 0], [0, - 1]]
+    x, y, r = 0, 0, 0
+    d = 0
+
+    for i in command:
+        if i == 'S':
+            x = dr[r][0]
+            y = dr[r][1]
+            d = abs(x ** 2 + y ** 2)
+            if d < dd:
+                d = dd
+        elif i == 'R':
+            d += 1
+            if d == 4:
+                d = 0
+        elif i == 'C':
+            d -= 1
+            if d == - 1:
+                d = 3
+    print(dd)
