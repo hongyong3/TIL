b214c240ad17508125c24d4b72efe914cc6726ca
hongyong3 <chy66822495@gmail.com>
2022-05-21 18:47:30 +0900

    D1121 | 20220521 PM | Algorithm

--- Algorithm/Swea/D4_14362.py
+++ Algorithm/Swea/D4_14362.py
@@ -13,25 +13,27 @@ L : 반시계방향
 R : 시계방향
 '''
 
-
+# 88 / 125
 dx = [1, 0, - 1, 0]
 dy = [0, - 1, 0, 1]
+
 T = int(input())
 for test_case in range(T):
     command = input()
-    ans = 'oo'
-    distance = 0
+    x = y = r = d = s = distance = 0
 
-    x, y, r, d = 0, 0, 0, 0
-
-    for i in command:
-        if i == 'S':
-            x += dx[r]
-            y += dy[r]
-            distance = abs(x ** 2 + y ** 2)
-            if d < distance:
-                d = distance
-        elif i == 'R':
-            r = (r + 1) % 4
-        elif i == 'L':
-            r = (r - 1) % 4
+    for a in range(4):
+        for i in command:
+            if i == 'S':
+                x += dx[r]
+                y += dy[r]
+                s += 1
+                distance = x ** 2 + y ** 2
+                if d < distance:
+                    d = distance
+            elif i == 'R':
+                r = (r + 1) % 4
+            elif i == 'L':
+                r = (r - 1) % 4
+    ans = 'oo' if s ** 2 == d != 0 else d
+    print("#{} {}".format(test_case + 1, ans))
