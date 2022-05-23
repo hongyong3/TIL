bfa186756241f702f964773e593ce36b30930090
hongyong3 <chy66822495@gmail.com>
2022-05-23 21:29:05 +0900

    D1123 | 20220523 PM | Algorithm

--- Algorithm/Swea/D4_14362.py
+++ Algorithm/Swea/D4_14362.py
@@ -7,18 +7,18 @@ dy = [0, 1, 0, - 1]
 T = int(input())
 for test_case in range(T):
     command = input()
-    x, y, k, ans = 0, 0, 0, 0
+    x, y, d, ans = 0, 0, 0, 0
     arr = set()
     for _ in range(4):
         for i in command:
             if i == 'S':
-                x += dx[k]
-                y += dy[k]
+                x += dx[d]
+                y += dy[d]
                 ans = max(ans, x ** 2 + y ** 2)
             elif i == 'R':
-                k = (k - 1) % 4
+                d = (d - 1) % 4
             elif i == 'L':
-                k = (k + 1) % 4
+                d = (d + 1) % 4
         arr.add(ans)
     if len(arr) == 4:
         ans = 'oo'
