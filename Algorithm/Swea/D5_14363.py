339912d21e64565c246b73aaafbd7e10e06668a9
hongyong3 <chy66822495@gmail.com>
2022-05-25 21:59:52 +0900

    D1125 | 20220525 PM | Algorithm

---
+++ Algorithm/Swea/D5_14363.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("D5_14363_input.txt", "r")
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    arr = [list(input()) for _ in range(N)]
+    p = q = [i for i in range(N)]
+    a = [[''] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if p[i] and q[j]:
+                a[i][j] = '#'
+            else:
+                a[i][j] = '.'
