b5f8582252f1de5fa4e67920393925a3c7566b8d
hongyong3 <chy66822495@gmail.com>
2022-06-10 22:40:27 +0900

    D1136 | 20220610 PM | Algorithm

--- Algorithm/Swea/D4_14450.py
+++ Algorithm/Swea/D4_14450.py
@@ -1,22 +1,22 @@
 import sys
 sys.stdin = open("D4_14450_input.txt", "r")
 
+# 40 / 87
 T = int(input())
 for test_case in range(T):
     L, R, Q = input().split()
-    # arr = list(map(int, input().split()))
     arr = list(input().split())
     ans = ''
-
+    idx = max(len(L), len(R))
     for i in arr:
-        for j in range(len(i)):
-            if int(L[j]) <= int(i[0]) <= int(R[j]):
-                temp = 1
-            else:
-                temp = 0
-                continue
-        if temp == 1:
-            ans += 'O'
-        else:
+        if len(i) > idx or int(i) > int(R):
             ans += 'X'
-    print(ans)
+            continue
+        for k in range(len(i)):
+            a, b, c = int(L[: k + 1]), int(i[: k + 1]), int(R[: k + 1])
+            if not (a <= b <= c):
+                ans += 'X'
+                break
+        else:
+            ans += 'O'
+    print("#{} {}".format(test_case + 1, ans))
