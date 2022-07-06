51dd095795d494db3b0331030886de43a552d9d5
hongyong3 <chy66822495@gmail.com>
2022-07-06 17:30:43 +0900

    D1156 | 20220706 PM | Algorithm

--- Algorithm/Swea/D4_14450.py
+++ Algorithm/Swea/D4_14450.py
@@ -14,30 +14,30 @@ for test_case in range(T):
             continue
         jdx = 0
         flag = True
-        while jdx < max(len(i), len(L)):
-            a = int(L[: jdx + 1])
-            b = int(i[: jdx + 1])
-            if a > b:
-                flag = False
-            jdx += 1
-        for j in range(len(i)):
-            flag = True
-            b = int(i[: j + 1])
-            # l과 b 비교
-            jdx = 0
-            while jdx < max(len(i), len(L)):
-                if int(j)
-            for k in range(len(L)):
-                pass
-            # r과 b 비교
-            for k in range(len(R)):
-                pass
-
-            a, c = int(L[: j + 1]), int(R[: j + 1])
-            if not (a <= b <= c):
-                ans += 'X'
-                break
-        else:
-            ans += 'O'
-            
-    print("#{} {}".format(test_case + 1, ans))
+    #     while jdx < max(len(i), len(L)):
+    #         a = int(L[: jdx + 1])
+    #         b = int(i[: jdx + 1])
+    #         if a > b:
+    #             flag = False
+    #         jdx += 1
+    #     for j in range(len(i)):
+    #         flag = True
+    #         b = int(i[: j + 1])
+    #         # l과 b 비교
+    #         jdx = 0
+    #         while jdx < max(len(i), len(L)):
+    #             if int(j)
+    #         for k in range(len(L)):
+    #             pass
+    #         # r과 b 비교
+    #         for k in range(len(R)):
+    #             pass
+    #
+    #         a, c = int(L[: j + 1]), int(R[: j + 1])
+    #         if not (a <= b <= c):
+    #             ans += 'X'
+    #             break
+    #     else:
+    #         ans += 'O'
+    #
+    # print("#{} {}".format(test_case + 1, ans))
