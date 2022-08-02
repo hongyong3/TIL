9724b1f4cc8d34b3a69af73f2562f419723b9c0d
hongyong3 <chy66822495@gmail.com>
2022-08-02 23:02:59 +0900

    D1172 | 20220802 PM | Algorithm

--- Algorithm/Swea/D3_14512.py
+++ Algorithm/Swea/D3_14512.py
@@ -4,13 +4,11 @@ sys.stdin = open("D3_14512_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     N, M = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    # arr = [list(map(int, input().split())) for _ in range(N)]
     time = [0] * M
     print("#{} {} {}".format(test_case + 1, N, M))
-    for i in arr:
-        s, e = i
-    # for _ in range(N):
-    #     s, e = map(int, input().split())
+    for _ in range(N):
+        s, e = map(int, input().split())
         for t in range(s - 1, e):
             time[t] += 1
     print(time)
