bf6f7c386bef106c315d9af657e007f6c5928bd4
hongyong3 <chy66822495@gmail.com>
2022-08-05 23:28:50 +0900

    D1174 | 20220805 PM | Algorithm

--- Algorithm/Swea/D2_14510.py
+++ Algorithm/Swea/D2_14510.py
@@ -4,9 +4,65 @@ sys.stdin = open("D2_14510_input.txt", "r")
 T = int(input())
 for test_case in range(T):
     N = int(input())
-    arr = list(map(int, input().split()))
-    high = max(arr)
-    ans = 0
-    for i in arr:
-        ans += high - i
+    arr = sorted(list(map(int, input().split())))
+    high = arr[- 1]
+    ans, idx = 0, 0
+    while idx < len(arr):
+        k = high - arr[idx]
+        if k % 3 == 0:
+            ans += (k // 3) * 2
+            arr.pop(idx)
+        else:
+            arr[idx] = high - arr[idx]
+            if arr[idx] > 6:
+                ans += (arr[idx] // 3) * 2
+                arr[idx] %= 3
+            idx += 1
+    arr = sorted(arr)
+    if len(arr) == 1:
+        ans += (arr[0] // 3) + arr[0] % 3
+        arr.pop()
+    nam1, idx = 0, 0
+    idx = 0
+    while len(arr) > 1 or idx < len(arr):
+        if arr[idx] == 1:
+            nam1 += 1
+            idx += 1
+        elif arr[idx] % 3 == 2:
+            if nam1:
+                arr[idx - 1] -= 1
+                if arr[idx - 1] == 0:
+                    arr.pop(idx - 1)
+                arr[idx - 1] -= 2
+                if arr[idx - 1] == 0:
+                    arr.pop(idx - 1)
+                nam1 -= 1
+                ans += 2
+                idx -= 1
+            else:
+                break
+                idx += 1
+        elif arr[idx] % 3 == 0:
+            ans += (arr[idx] // 3) * 2
+            arr.pop(idx)
+            idx -= 1
+        else:
+            if nam1:
+                arr[idx - 1] -= 1
+                if arr[idx - 1] == 0:
+                    arr.pop(idx - 1)
+                arr[idx - 1] -= 2
+                if arr[idx - 1] == 0:
+                    arr.pop(idx - 1)
+                ans += 2
+                idx -= 1
+    ans += (sum(arr) // 3) * 2 + sum(arr) % 3
+    # if len(arr) == 1:
+    #     if arr[0] % 3 == 1:
+    #         ans += (arr[0] // 3) * 2 + 1
+    #     elif arr[0] % 3 == 2:
+    #         ans += (arr[0] // 3) * 2 + 2
+    #     else:
+    #         ans += 2
+    #     arr.pop()
     print("#{} {}".format(test_case + 1, ans))
--- Algorithm/Swea/D3_14512.py
+++ Algorithm/Swea/D3_14512.py
@@ -7,8 +7,7 @@ for test_case in range(T):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
     arr = list(permutations(arr))
-    ans = float('inf')
-    chk = 0
+    ans = 31
 
     for i in arr:
         num = i[0][0]
@@ -21,30 +20,12 @@ for test_case in range(T):
             if i[idx][0] <= temp <= i[idx][1]:
                 temp += 1
                 idx += 1
-            elif temp < i[idx][0]:
+            else:
                 num += 1
                 temp = num + 1
                 idx = 1
-            elif temp > i[idx][1]:
-                break
         if num != - 1:
             ans = min(ans, num)
-    # print(ans)
-    # for i in range(len(arr)):
-    #     num = arr[i][0][0]
-    #     temp = num + 1
-    #     idx = 1
-    #     while idx < N:
-    #         if not (arr[i][0][0] <= num <= arr[i][0][1]):
-    #             num = - 1
-    #             break
-    #         if arr[idx][0][0] <= temp <= arr[idx][0][1]:
-    #             temp += 1
-    #             idx += 1
-    #         else:
-    #             num += 1
-    #             temp = num + 1
-    #             idx = 1
-    #     if num != - 1:
-    #         ans = min(ans, num)
-    print(ans)
+    if ans == 31:
+        ans = - 1
+    print("#{} {}".format(test_case + 1, ans))
