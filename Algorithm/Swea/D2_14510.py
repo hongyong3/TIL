c47310d45edf750a016b4916d6e004f07c49c5d7
hongyong3 <chy66822495@gmail.com>
2022-08-07 23:41:24 +0900

    D1176 | 20220807 PM | Algorithm

--- Algorithm/Swea/D2_14510.py
+++ Algorithm/Swea/D2_14510.py
@@ -1,99 +1,86 @@
 import sys
 sys.stdin = open("D2_14510_input.txt", "r")
 
-# T = int(input())
-# for test_case in range(T):
-#     N = int(input())
-#     arr = sorted(list(map(int, input().split())))
-#     high = arr[- 1]
-#     ans, idx = 0, 0
-#     while idx < len(arr):
-#         k = high - arr[idx]
-#         if k % 3 == 0:
-#             ans += (k // 3) * 2
-#             arr.pop(idx)
-#         else:
-#             arr[idx] = high - arr[idx]
-#             if arr[idx] > 6:
-#                 ans += (arr[idx] // 3) * 2
-#                 arr[idx] %= 3
-#             idx += 1
-#     arr = sorted(arr)
-#     if len(arr) == 1:
-#         ans += (arr[0] // 3) + arr[0] % 3
-#         arr.pop()
-#     nam1, idx = 0, 0
-#     idx = 0
-#     while len(arr) > 1 or idx < len(arr):
-#         if arr[idx] == 1:
-#             nam1 += 1
-#             idx += 1
-#         elif arr[idx] % 3 == 2:
-#             if nam1:
-#                 arr[idx - 1] -= 1
-#                 if arr[idx - 1] == 0:
-#                     arr.pop(idx - 1)
-#                 arr[idx - 1] -= 2
-#                 if arr[idx - 1] == 0:
-#                     arr.pop(idx - 1)
-#                 nam1 -= 1
-#                 ans += 2
-#                 idx -= 1
-#             else:
-#                 break
-#                 idx += 1
-#         elif arr[idx] % 3 == 0:
-#             ans += (arr[idx] // 3) * 2
-#             arr.pop(idx)
-#             idx -= 1
-#         else:
-#             if nam1:
-#                 arr[idx - 1] -= 1
-#                 if arr[idx - 1] == 0:
-#                     arr.pop(idx - 1)
-#                 arr[idx - 1] -= 2
-#                 if arr[idx - 1] == 0:
-#                     arr.pop(idx - 1)
-#                 ans += 2
-#                 idx -= 1
-#     ans += (sum(arr) // 3) * 2 + sum(arr) % 3
-#     # if len(arr) == 1:
-#     #     if arr[0] % 3 == 1:
-#     #         ans += (arr[0] // 3) * 2 + 1
-#     #     elif arr[0] % 3 == 2:
-#     #         ans += (arr[0] // 3) * 2 + 2
-#     #     else:
-#     #         ans += 2
-#     #     arr.pop()
-#     print("#{} {}".format(test_case + 1, ans))
-
+# 49 // 50
 T = int(input())
 for test_case in range(T):
     N = int(input())
     data = list(map(int, input().split()))
     high = max(data)
-    ans, h, j = 0, 0, 0
-    arr = []
+    ans, n1, n2, n4, n5 = 0, 0, 0, 0, 0
     for i in data:
         k = high - i
         if k % 3:
             if k > 6:
-                ans += (k // 3) * 2
-                k %= 3
+                ans += (k // 6) * 4
+                k %= 6
             if k == 1:
-                h += 1
+                n1 += 1
             elif k == 2:
-                j += 1
+                n2 += 1
+            elif k == 4:
+                n4 += 1
             else:
-                arr.append(k)
+                n5 += 1
         else:
             ans += (k // 3) * 2
-    m = min(h, j)
-    ans += 2 * m
-    arr = sorted([1] * (h - m) + [2] * (j - m) + arr)
 
-    if arr:
-        # 6 : [1, 1, 4], [1, 5], [2, 4] 9 : [4, 5]
-        print("#{} {}".format(test_case + 1, arr))
-        print(ans)
-        print(sum(arr))
+    m = min(n1, n2)
+    ans += 2 * m
+    n1 -= m
+    n2 -= m
+
+    if n1:
+        if n5:
+            m = min(n1, n5)
+            ans += 4 * m
+            n1 -= m
+            n5 -= m
+        if n4:
+            m = min(n1 // 2, n4)
+            ans += 4 * m
+            n1 -= 2 * m
+            n4 -= m
+        if n1:
+            ans += (2 * n1) - 1
+            n1 -= n1
+
+    if n2:
+        if n4:
+            m = min(n2, n4)
+            ans += 4 * m
+            n2 -= m
+            n4 -= m
+        if n5:
+            m = min(n2 // 2, n5)
+            ans += 6 * m
+            n2 -= 2 * m
+            n5 -= m
+        if n2:
+            ans += (n2 // 3) * 4
+            if n2 % 3 == 1:
+                ans += 2
+            elif n2 % 3 == 2:
+                ans += 3
+
+    if n4 and n5:
+        m = min(n4, n5)
+        ans += 6 * m
+        n4 -= m
+        n5 -= m
+
+    if n4:
+        ans += (n4 // 3) * 8
+        if n4 % 3 == 1:
+            ans += 3
+        elif n4 % 3 == 2:
+            ans += 6
+
+    if n5:
+        ans += (n5 // 3) * 10
+        if n5 % 3 == 1:
+            ans += 4
+        elif n5 % 3 == 2:
+            ans += 7
+
+    print("#{} {}".format(test_case + 1, ans))
