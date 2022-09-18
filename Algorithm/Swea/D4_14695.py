6c34bba481856a7e59d2d9c20449e39af8978f41
hongyong3 <chy66822495@gmail.com>
2022-09-18 18:12:52 +0900

    D1205 | 20220918 PM | Algorithm

--- Algorithm/Swea/D4_14695.py
+++ Algorithm/Swea/D4_14695.py
@@ -50,28 +50,28 @@ D = x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3 * (y1 * z2 - y2 * z
 #     print(zp)
 #     # print("#{} {}".format(test_case + 1, ans))
 
-def v(x1, y1, z1, x2, y2, z2, x3, y3, z3):
-    A = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2)
-    B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2)
-    C = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
-    D = - (x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3 * (y1 * z2 - y2 * z1))
-    return A, B, C, D
-
-T = int(input())
-for test_case in range(T):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    ans = "TAK"
-
-    if N > 3:
-        chk1 = 1
-        a, b, c, d = v(*arr[0], *arr[1], *arr[2])
-        for x, y, z in arr:
-            n = a * x + b * y + c * z + d
-            if n:
-                ans = "NIE"
-                chk1 = 0
-                break
+# def v(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+#     A = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2)
+#     B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2)
+#     C = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
+#     D = - (x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3 * (y1 * z2 - y2 * z1))
+#     return A, B, C, D
+#
+# T = int(input())
+# for test_case in range(T):
+#     N = int(input())
+#     arr = [list(map(int, input().split())) for _ in range(N)]
+#     ans = "TAK"
+#
+#     if N > 3:
+#         chk1 = 1
+#         a, b, c, d = v(*arr[0], *arr[1], *arr[2])
+#         for x, y, z in arr:
+#             n = a * x + b * y + c * z + d
+#             if n:
+#                 ans = "NIE"
+#                 chk1 = 0
+#                 break
 
         # if chk1:
         #     chk2, idx = 0, 0
@@ -115,4 +115,26 @@ for test_case in range(T):
         #                 if i[2] != z1:
         #                     ans = "NIE"
         #                     break
+    # print("#{} {}".format(test_case + 1, ans))
+
+# 27 / 29
+def v(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+    A = y1 * (z2 - z3) + y2 * (z3 - z1) + y3 * (z1 - z2)
+    B = z1 * (x2 - x3) + z2 * (x3 - x1) + z3 * (x1 - x2)
+    C = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
+    D = x1 * (y2 * z3 - y3 * z2) + x2 * (y3 * z1 - y1 * z3) + x3 * (y1 * z2 - y2 * z1)
+    return A, B, C, D
+
+
+T = int(input())
+for test_case in range(T):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    ans = "TAK"
+    if N > 3:
+        a, b, c, d = v(*arr[0], *arr[1], *arr[2])
+        for x, y, z in arr:
+            if a * x + b * y + c * z - d:
+                ans = "NIE"
+                break
     print("#{} {}".format(test_case + 1, ans))
