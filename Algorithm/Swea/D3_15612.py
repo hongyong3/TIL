a8b96941c5a35abee2c0f9ed3fb1f058b6a855a3
hongyong3 <chy66822495@gmail.com>
2022-10-19 22:58:31 +0900

    D1222 | 20221019 PM | Algorithm

--- Algorithm/Swea/D3_15612.py
+++ Algorithm/Swea/D3_15612.py
@@ -3,17 +3,20 @@ sys.stdin = open("D3_15612_input.txt", "r")
 
 T = int(input())
 for test_case in range(T):
-    arr = [list(input()) for _ in range(8)]
+    arr = [input() for _ in range(8)]
     arrT = list(zip(*arr))
     ans = "yes"
+
     for i in range(8):
-        cnt, cntT = 0, 0
+        cnt = 0
         for j in range(8):
-            if arr[i][j] == 'O':
-                cnt += 1
             if arrT[i][j] == 'O':
-                cntT += 1
-        if cnt > 1 or cntT > 1:
+                cnt += 1
+        if cnt > 1:
+            ans = "no"
+            break
+    for i in arr:
+        if arr.count('O') > 1:
             ans = "no"
             break
     print("#{} {}".format(test_case + 1, ans))
