616b8f6834c972fb439d58d7bacebe2b84ed42af
hongyong3 <chy66822495@gmail.com>
2022-11-06 22:08:53 +0900

    D1232 | 20221106 PM | Algorithm

--- Algorithm/Swea/D4_15623.py
+++ Algorithm/Swea/D4_15623.py
@@ -50,26 +50,21 @@ sys.stdin = open("D4_15623_input.txt", "r")
 #     print("#{} {}".format(test_case + 1, ans))
 
 # 249 // 266 Fail
-def solve(idx, nx, ny):
+def solve1(idx, nx, ny):
     global ans
     if idx == 2:
         ans = min(ans, nx * ny)
         return
 
-
     for k in range(1, N + 1):
         if graph[idx][k] != 0 and not visited[k]:
+            if ans <= (nx + graph[idx][k][0]) * (ny + graph[idx][k][1]):
+                continue
             visited[k] = 1
             nx += graph[idx][k][0]
             ny += graph[idx][k][1]
-            if ans < nx * ny:
-                nx -= graph[idx][k][0]
-                ny -= graph[idx][k][1]
-                visited[k] = 0
-                continue
-            solve(k, nx, ny)
+            solve1(k, nx, ny)
             visited[k] = 0
-
 
 T = int(input())
 for test_case in range(T):
@@ -82,7 +77,6 @@ for test_case in range(T):
         A, B, X, Y = map(int, input().split())
         graph[A][B] = [X, Y]
         graph[B][A] = [X, Y]
-
     chk1, chk2 = 0, 0
     for i in range(1, N + 1):
         if graph[1][i] != 0:
@@ -94,13 +88,14 @@ for test_case in range(T):
         ans = float('inf')
         visited = [0] * (N + 1)
         visited[1] = 1
-        for j in range(1, N + 1):
+        for j in range(2, N + 1):
             if graph[1][j] != 0:
                 visited[j] = 1
                 numX, numY = graph[1][j][0], graph[1][j][1]
-                solve(j, numX, numY)
+                if ans > numX * numY:
+                    solve1(j, numX, numY)
                 visited[j] = 0
     else:
         ans = - 1
-        
+
     print("#{} {}".format(test_case + 1, ans))
