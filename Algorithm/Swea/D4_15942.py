8c3f1d97b2e2a0c74b61245f9368e955adbcae8a
hongyong3 <chy66822495@gmail.com>
2023-01-19 22:30:01 +0900

    D1263 | 20230119 PM | Algorithm

--- Algorithm/Swea/D4_15942.py
+++ Algorithm/Swea/D4_15942.py
@@ -1,20 +1,49 @@
 import sys
 sys.stdin = open("D4_15942_input.txt", "r")
 
-# 14 / 29
+# 9 / 29 Runtime Error
+# T = int(input())
+# for test_case in range(T):
+#     N, K = map(int, input().split())
+#     arr = sorted(list(map(int, input().split())))
+#     ans = 0
+#     total = sum(arr)
+#
+#     '''
+#     - 처음 보유한 함선이 0인 경우
+#     - 처음 보유한 함선이 arr[0]보다 작은 경우
+#     - (처음 보유한 함선) + (모든 행성의 인구 - A_n) < A_n
+#     '''
+#
+#     if K == 0 or K < arr[0] or K + total < arr[- 1] * 2:
+#         ans = - 1
+#     else:
+#         idx = 0
+#         while idx < len(arr):
+#             if K < arr[idx]:
+#                 ans += 1
+#                 K += arr[idx - 1]
+#                 total -= arr[idx - 1]
+#                 arr.pop(idx - 1)
+#                 idx -= 1
+#             else:
+#                 idx += 1
+#
+#         if K < sum(arr):
+#             ans += 1
+#             K += arr[- 1]
+#             total -= arr[- 1]
+#     print("#{} {}".format(test_case + 1, ans))
+
+
+# 27 / 29 Fail
 T = int(input())
 for test_case in range(T):
     N, K = map(int, input().split())
     arr = sorted(list(map(int, input().split())))
-    ans = 0
     total = sum(arr)
+    ans = 0
 
-    '''
-    - 처음 보유한 함선이 0인 경우
-    - 처음 보유한 함선이 arr[0]보다 작은 경우
-    - (처음 보유한 함선) + (모든 행성의 인구 - A_n) < A_n
-    '''
-    
     if K == 0 or K < arr[0] or K + total < arr[- 1] * 2:
         ans = - 1
     else:
@@ -23,11 +52,15 @@ for test_case in range(T):
             if K < arr[idx]:
                 ans += 1
                 K += arr[idx - 1]
+                total -= arr[idx - 1]
                 arr.pop(idx - 1)
-                idx -= 1
             else:
                 idx += 1
-
-        if K < sum(arr):
+        while K < total:
+        # if K < total:
             ans += 1
+            K += arr[- 1]
+            total -= arr[- 1]
+            arr.pop(- 1)
+
     print("#{} {}".format(test_case + 1, ans))
