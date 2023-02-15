1558a44050aca94f4c4668c16ba19f4a0958607e
hongyong3 <chy66822495@gmail.com>
2023-02-15 23:16:22 +0900

    D1274 | 20230215 PM | Algorithm

--- Algorithm/Swea/D4_15942.py
+++ Algorithm/Swea/D4_15942.py
@@ -34,8 +34,8 @@ sys.stdin = open("D4_15942_input.txt", "r")
 #             K += arr[- 1]
 #             total -= arr[- 1]
 #     print("#{} {}".format(test_case + 1, ans))
-
-
+#
+#
 # 27 / 29 Fail
 # 새로 짜야 할듯..
 # T = int(input())
@@ -81,43 +81,88 @@ sys.stdin = open("D4_15942_input.txt", "r")
 #     print("#{} {}".format(test_case + 1, ans))
 
 # 22 / 29 Fail
-def upperBound(k, e):
-    global N, K, s, total, visited
+# def upperBound(k, e):
+#     global N, K, s, total, visited
+#     while s < e:
+#         m = (s + e) // 2
+#         if k < arr[m]:
+#             e = m
+#         else:
+#             s = m + 1
+#     if not visited[s - 1]:
+#         K += arr[s - 1]
+#         total -= arr[s - 1]
+#         visited[s - 1] = 1
+#     else:
+#         idx = 1
+#         while True:
+#             if not visited[s - 1 - idx]:
+#                 K += arr[s - 1 - idx]
+#                 total -= arr[s - 1 - idx]
+#                 visited[s- 1 - idx] = 1
+#                 break
+#             else:
+#                 idx += 1
+#     return s
+#
+# T = int(input())
+# for test_case in range(T):
+#     N, K = map(int, input().split())
+#     arr = sorted(list(map(int, input().split())))
+#     total = sum(arr)
+#     if K < arr[0] or K + total < arr[- 1] * 2:
+#         ans = - 1
+#     else:
+#         ans = 0
+#         visited = [0] * N
+#         s = 0
+#         while K < total:
+#             arr[upperBound(K, N - 1)]
+#             ans += 1
+#
+#     print("#{} {}".format(test_case + 1, ans))
+
+
+def upperBound(k):
+    s, e = 0, N - 1
     while s < e:
         m = (s + e) // 2
-        if k < arr[m]:
-            e = m
-        else:
+        if arr[m] <= k:
             s = m + 1
-    if not visited[s - 1]:
-        K += arr[s - 1]
-        total -= arr[s - 1]
-        visited[s - 1] = 1
+        else:
+            e = m
+    return e
+
+
+def find(v):
+    if point[v] == v:
+        return v
     else:
-        idx = 1
-        while True:
-            if not visited[s - 1 - idx]:
-                K += arr[s - 1 - idx]
-                total -= arr[s - 1 - idx]
-                visited[s- 1 - idx] = 1
-                break
-            else:
-                idx += 1
-    return s
+        point[v] = find(point[v])
 
 T = int(input())
 for test_case in range(T):
     N, K = map(int, input().split())
     arr = sorted(list(map(int, input().split())))
     total = sum(arr)
-    if K < arr[0] or K + total < arr[- 1] * 2:
-        ans = - 1
-    else:
-        ans = 0
-        visited = [0] * N
-        s = 0
-        while K < total:
-            arr[upperBound(K, N - 1)]
-            ans += 1
+    visited = [0] * N
+    point = [i for i in range(N + 1)]
+    ans = 0
+    while K < total:
+        idx = upperBound(K)
+        if idx:
+            val = find(idx) - 1
+            if visited[val]:
+                ans = - 1
+                break
 
+            K += arr[val]
+            total -= arr[val]
+            ans += 1
+            visited[val] = 1
+            if val:
+                point[val] = find(val)
+        else:
+            ans = - 1
+            break
     print("#{} {}".format(test_case + 1, ans))
