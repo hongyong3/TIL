7622487dad9c1c33eb7e9466bdc531994ee5d474
hongyong3 <chy66822495@gmail.com>
2023-03-09 22:06:02 +0900

    D1278 | 20230309 PM | Algorithm

--- Algorithm/Swea/D4_16801.py
+++ Algorithm/Swea/D4_16801.py
@@ -5,7 +5,14 @@ T = int(input())
 for test_case in range(T):
     N, K = map(int, input().split())
     arrA = sorted(list(map(int, input().split())))
-    arrF = sorted(list(map(int, input().split())))
+    arrF = sorted(list(map(int, input().split())))[::- 1]
+    arr = []
+    arrIdx = []
+    for i in range(N):
+        arr.append(arrA[i] * arrF[i])
+        # arr.append([arrA[i], arrF[i], arrA[i] * arrF[i]])
+    print(K)
     print(arrA)
     print(arrF)
-    
+    print(arr)
+    print()
