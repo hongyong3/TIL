40447dad11209b147a3cc38a2cf79cb5345fc06c
hongyong3 <chy66822495@gmail.com>
2023-04-26 22:58:00 +0900

    D1301 | 20230426 PM | Algorithm

--- Algorithm/Swea/D4_14450.py
+++ Algorithm/Swea/D4_14450.py
@@ -46,17 +46,42 @@ sys.stdin = open("D4_14450_input.txt", "r")
 #
 #     print(ans)
 
-# 자리수가 같을 경우
-def same():
-    pass
 
-# 자리수가 다를 경우
-def diff():
-    pass
+'''
+1. 자리수가 같을 경우
+    1) 만약 q가 r보다 크면 바로 X
+2. 자리수가 다를 경우
+'''
 
 T = int(input())
 for test_case in range(T):
     L, R, Q = input().split()
-    chklen = 1 if len(str(L)) == len(str(R)) else 0
     arr = list(input().split())
-    print(arr)
+    ans = ''
+    lr, rr = len(L), len(R)
+    # 자리수가 같을 경우
+    if rr - lr == 0:
+        for i in arr:
+            if len(i) > rr:
+                ans += 'X'
+                continue
+
+            temp = 'O'
+            for j in range(len(i)):
+                num = int(i[:j + 1])
+                if not(int(L[:j + 1]) <= num <= int(R[:j + 1])):
+                    ans += 'X'
+                    break
+            else:
+                ans += 'O'
+
+    # 자리수가 다를 경우
+    elif rr - lr <= 2:
+        pass
+    else:
+        for i in arr:
+            if len(i) > rr:
+                ans += 'X'
+                continue
+            ans += 'O'
+    print("#{} {}".format(test_case + 1, ans))
