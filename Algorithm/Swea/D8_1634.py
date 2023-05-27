6769a6546f5e25c8728063970aa5da2969d97ec4
hongyong3 <chy66822495@gmail.com>
2023-05-27 22:35:56 +0900

    D1313 | 20230527 PM | Algorithm

--- Algorithm/Swea/D8_1634.py
+++ Algorithm/Swea/D8_1634.py
@@ -1,20 +1,40 @@
 import sys
 sys.stdin = open("D8_1634_input.txt", "r")
 
+xy = 'abcdefghijklmnopqrstuvwxyz'
 T = int(input())
-for test_case in range(T):
+for test_case in range(1):
     M, N = map(int, input().split())
-    # translations = [input().split() for _ in range(M)]
-    dic = {}
+    alpha = [[0] * 26 for _ in range(26)]
+
     for _ in range(M):
         x, y = input().split()
-        if x not in dic:
-            dic[x] = y
-        else:
-            dic[x] += y
-        if y not in dic:
-            dic[y] = x
-        else:
-            dic[y] += x
+        # alpha[ord(x) - 97][ord(y) - 97] = 1
+        for i in range(26):
+            if alpha[ord(x) - 97][i] == 0:
+                alpha[ord(x) - 97][i] = y
+                break
 
-    letters = [list(input()) for _ in range(N)]
+    for i in range(26):
+        print(xy[i], alpha[i])
+
+
+
+    # for _ in range(M):
+    #     x, y = input().split()
+    #     if x not in dic:
+    #         dic[x] = y
+    #     else:
+    #         dic[x] += y
+    #     if y not in dic:
+    #         dic[y] = x
+    #     else:
+    #         dic[y] += x
+    #
+    # print("#{}".format(test_case + 1))
+    # for _ in range(M):
+    #     a, b = input().split()
+    #     if len(a) != len(b):
+    #         print("no")
+    #     # else:
+    #     #     for i in range(len(a)):
