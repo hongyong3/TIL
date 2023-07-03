f57fc22b70763dd23c969b4b253d8fe03b12c2fd
hongyong3 <chy66822495@gmail.com>
2023-07-03 21:54:14 +0900

    D1331 | 20230703 PM | Algorithm

--- Algorithm/Swea/D5_17324.py
+++ Algorithm/Swea/D5_17324.py
@@ -4,6 +4,5 @@ sys.stdin = open("D5_17324_input.txt", "r")
 
 T = int(input())
 for test_case in range(T):
-    N = int(input())
-    N, M = map(int, input().split())
-    arr = list(map(int, input().split()))
+    P = input()
+    Q = input()
