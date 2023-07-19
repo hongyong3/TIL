7ec5c78d74d728032ddf348dc649014288bdcd3c
hongyong3 <chy66822495@gmail.com>
2023-07-19 20:28:13 +0900

    D1336 | 20230719 PM | Algorithm

--- Algorithm/Swea/D4_17643.py
+++ Algorithm/Swea/D4_17643.py
@@ -9,19 +9,29 @@ for test_case in range(T):
     if X % 3 and Y % 3:
         print("#{} {}".format(test_case + 1, "no"))
         continue
-    if Y % 3:
+
+    if X > Y:
         X, Y = Y, X
-    if X % 3 == 2:
-        ans = "no"
+
+    if X % 3:
+        chk = 1 # chk이면 X에 1이 , 아니면 Y에 1이
+
     arr = []
     for i in range(20):
-        if max(X, Y) < 3 ** i:
-            break
         arr.append(3 ** i)
-    idx, jdx, chk = 0, len(arr), len(arr)
-    for i in range(len(arr)):
-        if X > arr[i]:
-            idx = i
+        if max(X, Y) <= 3 ** i:
+            if 3 ** i not in arr:
+                arr.append(3 ** i)
             break
-    print(idx, jdx)
+
+    idx, jdx = 0, len(arr)
+    for i in range(len(arr)):
+        if X < arr[i]:
+            break
+        idx += 1
+
+
     print(arr)
+    print(idx, jdx)
+    print(arr[:idx], arr[idx:jdx])
+    print()
