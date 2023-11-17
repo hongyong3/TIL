dd9d8123f6fe8035ba0dd4f8498d6991db135d54
hongyong3 <chy66822495@gmail.com>
2023-11-17 17:49:24 +0900

    D1375 | 20231117 PM | Algorithm

--- Algorithm/Swea/D4_19004.py
+++ Algorithm/Swea/D4_19004.py
@@ -2,17 +2,22 @@ import sys
 sys.stdin = open("D4_19004_input.txt", "r")
 
 T = int(input())
-for test_case in range(T):
+for test_case in range(1):
     N, K = map(int, input().split())
     num = set()
-    arr = []
-    for _ in range(N):
+    graph = []
+    coordinate = [[] for _ in range(K + 1)]
+
+    for x in range(N):
         data = list(map(int, input().split()))
-        arr.append(data)
-        for i in data:
-            num.add(i)
+        graph.append(data)
+        for y in range(N):
+            num.add(data[y])
+            coordinate[data[y]].append([x, y])
+
     if K != len(num):
         ans = - 1
     else:
-        ans = 0
-    print(ans)
+        ans = float('inf')
+        # 탐색 해보자..
+        # for i in range()
