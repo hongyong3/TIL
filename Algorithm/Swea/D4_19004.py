c220ee0ec6e84e5c3335183f508dc6d7b6e991dc
hongyong3 <chy66822495@gmail.com>
2023-11-24 19:19:04 +0900

    D1379 | 20231124 PM | Algorithm

--- Algorithm/Swea/D4_19004.py
+++ Algorithm/Swea/D4_19004.py
@@ -1,27 +1,27 @@
 import sys
 sys.stdin = open("D4_19004_input.txt", "r")
 
-def solve(k, dis):
-    global ans
-    if k == K:
-        if dis < ans:
-            ans = dis
-        else:
-            return
-
-    while
+# def solve(k, dis):
+#     global ans
+#     if k == K:
+#         if dis < ans:
+#             ans = dis
+#         else:
+#             return
+#
+#     while
 
 
 T = int(input())
-for test_case in range(1):
+for test_case in range(T):
     N, K = map(int, input().split())
     num = set()
-    graph = []
+    arr = []
     coordinate = [[] for _ in range(K + 1)]
 
     for x in range(N):
         data = list(map(int, input().split()))
-        graph.append(data)
+        arr.append(data)
         for y in range(N):
             num.add(data[y])
             coordinate[data[y]].append([x, y])
@@ -31,7 +31,8 @@ for test_case in range(1):
     else:
         ans = float('inf')
         # 탐색 해보자..  가지치기 필수
-        solve(1, 0)   # K, distance
+        # solve(1, 0)   # K, distance
         # for i in range(1, len(coordinate)):
         #     for j in range(len(coordinate[i])):
         #         x, y = coordinate[i][j][0], coordinate[i][j][1]
+    print(ans)
